e8de31c5ff500a9439ed30b90bdb6a4105002c80
qinmingyuan <mingyuan0715@foxmail.com>
2019-05-02 21:34:39 +0800

    fix list scope

--- app/models/rails_wechat/wechat_feedback.rb
+++ app/models/rails_wechat/wechat_feedback.rb
@@ -2,7 +2,7 @@ module RailsWechat::WechatFeedback
   extend ActiveSupport::Concern
   included do
     attribute :feedback_on, :date, default: -> { Date.today }
-    acts_as_list scope: :feedback_on
+    acts_as_list scope: [:feedback_on]
     
     belongs_to :wechat_user
   end
