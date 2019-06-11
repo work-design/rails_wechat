ae26c08c86ebd5a3caf2a2603eb0700b380464a2
qinmingyuan <mingyuan0715@foxmail.com>
2019-06-11 19:51:25 +0800

    wechat config js helper

--- lib/wechat/helpers.rb
+++ lib/wechat/helpers.rb
@@ -1,11 +1,9 @@
 module Wechat
   module Helpers
     
-    def wechat_raw_config_js(config_options = {})
-      account = config_options[:account]
-      
-      app = Wechat.config(account)
-      api = Wechat.api(account)
+    def wechat_raw_config_js(wechat_config_id: , debug: false, apis: [])
+      app = Wechat.config(wechat_config_id)
+      api = Wechat.api(wechat_config_id)
 
       if app.trusted_domain_fullname
         page_url = "#{app.trusted_domain_fullname}#{controller.request.original_fullpath}"
@@ -18,18 +16,18 @@ module Wechat
 
       <<-WECHAT_CONFIG_JS
 wx.config({
-  debug: #{config_options[:debug]},
+  debug: #{debug},
   appId: '#{app.appid}',
   timestamp: '#{js_hash[:timestamp]}',
   nonceStr: '#{js_hash[:noncestr]}',
   signature: '#{js_hash[:signature]}',
-  jsApiList: ['#{config_options[:api].join("','")}']
+  jsApiList: ['#{apis.join("','")}']
 });
 WECHAT_CONFIG_JS
     end
 
-    def wechat_config_js(config_options = {})
-      config_js = wechat_raw_config_js(config_options)
+    def wechat_config_js(wechat_config_id: , debug: false, apis: [])
+      config_js = wechat_raw_config_js(wechat_config_id: wechat_config_id, debug: debug, apis: apis)
       javascript_tag config_js, type: 'application/javascript'
     end
     
