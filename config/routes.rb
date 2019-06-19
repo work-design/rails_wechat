617f70343ae986c33f2cde15ca35d35857a8e3b0
qinmingyuan <mingyuan0715@foxmail.com>
2019-06-19 18:15:49 +0800

    support default menu

--- config/routes.rb
+++ config/routes.rb
@@ -13,17 +13,17 @@ Rails.application.routes.draw do
     resources :wechat_configs do
       get 'help' => :edit_help, on: :member
       get :info, on: :member
-      resources :wechat_menus do
-        collection do
-          get :new_parent
-          post :sync
-        end
-        get :edit_parent, on: :member
-      end
       resources :wechat_responses do
         post :sync, on: :member
       end
       resources :wechat_feedbacks, except: [:new, :create]
+    end
+    resources :wechat_menus do
+      collection do
+        get :new_parent
+        post :sync
+      end
+      get :edit_parent, on: :member
     end
     resources :extractors
   end
