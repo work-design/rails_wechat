3e7556cc756bc3c2436a45efc9e8a1174474c5a2
qinmingyuan <mingyuan0715@foxmail.com>
2019-06-26 17:31:58 +0800

    wechat account module

---
+++ app/models/rails_wechat/effective_module.rb
@@ -0,0 +1,29 @@
+module RailsWechat::EffectiveModule
+  
+  def invoke_effect(wechat_user)
+    p 'please implement in class'
+  end
+
+  def qrcode_later
+    WechatQrcodeJob.perform_later(self)
+  end
+  
+  def wechat_config
+    if WechatConfig.column_names.include?('organ_id')
+      WechatConfig.find_by(organ_id: nil, primary: true)
+    else
+      WechatConfig.find_by(primary: true)
+    end
+  end
+
+  def qrcode
+    unless wechat_response
+      create_wechat_response(type: 'TempScanResponse', wechat_config_id: wechat_config.id) if wechat_config
+    end
+  end
+
+  def qrcode_url
+    wechat_response&.qrcode_file_url
+  end
+  
+end
