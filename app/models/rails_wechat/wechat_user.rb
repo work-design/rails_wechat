7206944379c3127df157d67d756845e5fa47fcaf
qinmingyuan <mingyuan0715@foxmail.com>
2019-07-01 10:10:58 +0800

    no response items

--- app/models/rails_wechat/wechat_user.rb
+++ app/models/rails_wechat/wechat_user.rb
@@ -2,7 +2,7 @@ module RailsWechat::WechatUser
   extend ActiveSupport::Concern
   included do
     has_many :wechat_requests, dependent: :delete_all
-    has_many :response_items, dependent: :delete_all
+    has_many :ticket_items, dependent: :delete_all
   end
   
 end
