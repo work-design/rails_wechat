b729c723d8879cb5c6ed92251f995f56d6b6d2d0
qinmingyuan <mingyuan0715@foxmail.com>
2019-07-05 15:52:12 +0800

    default tag for wechat user

--- app/models/rails_wechat/wechat_user.rb
+++ app/models/rails_wechat/wechat_user.rb
@@ -7,6 +7,15 @@ module RailsWechat::WechatUser
     has_many :ticket_items, dependent: :delete_all
     has_many :wechat_user_tags, dependent: :destroy
     has_many :wechat_tags, through: :wechat_user_tags
+    
+    after_save_commit :sync_default_wechat_tag, if: -> { saved_change_to_app_id? }
+  end
+  
+  def sync_default_wechat_tag
+    wechat_tag_default = WechatTagDefault.find_by(default_type: self.class.name)
+    tag = wechat_app.wechat_tags.find_by(wechat_tag_default_id: wechat_tag_default.id)
+    
+    self.wechat_user_tags.create(tag_id: tag.id)
   end
   
 end
