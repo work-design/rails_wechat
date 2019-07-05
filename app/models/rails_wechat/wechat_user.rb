dc461748d83bc233438d15a587e6ff03860fe4df
qinmingyuan <mingyuan0715@foxmail.com>
2019-07-05 15:34:03 +0800

    sync user tag id

--- app/models/rails_wechat/wechat_user.rb
+++ app/models/rails_wechat/wechat_user.rb
@@ -1,6 +1,8 @@
 module RailsWechat::WechatUser
   extend ActiveSupport::Concern
   included do
+    belongs_to :wechat_app, foreign_key: :app_id, primary_key: :appid
+    
     has_many :wechat_requests, dependent: :delete_all
     has_many :ticket_items, dependent: :delete_all
     has_many :wechat_user_tags, dependent: :destroy
--- app/models/rails_wechat/wechat_user_tag.rb
+++ app/models/rails_wechat/wechat_user_tag.rb
@@ -3,9 +3,24 @@ module RailsWechat::WechatUserTag
   included do
     belongs_to :wechat_user
     belongs_to :wechat_tag
+    
+    after_create_commit :sync_to_wechat
+    after_destroy_commit :remove_from_wechat
   end
 
- 
+  def sync_to_wechat
+    if wechat_api
+      wechat_api.tag_add_user(wechat_tag.tag_id, wechat_user.uid)
+    end
+  end
+  
+  def wechat_api
+    @wechat_api ||= wechat_user.wechat_app&.api
+  end
+  
+  def remove_from_wechat
+    wechat_api.tag_del_user(wechat_tag.tag_id, wechat_user.uid) if wechat_api
+  end
   
 end
 
