725f1d68c78d2df0bfe84e682f5b87445afe8b0e
qinmingyuan <mingyuan0715@foxmail.com>
2019-07-14 00:07:52 +0800

    use wechat app

--- app/models/rails_wechat/wechat_request/text_request.rb
+++ app/models/rails_wechat/wechat_request/text_request.rb
@@ -1,15 +1,15 @@
 module RailsWechat::WechatRequest::TextRequest
   
   def response
-    res = "#{wechat_config.help}"
-    return res unless body.match? Regexp.new(wechat_config.match_values)
-    res = wechat_config.text_responses.map do |wr|
+    res = "#{wechat_app.help}"
+    return res unless body.match? Regexp.new(wechat_app.match_values)
+    res = wechat_app.text_responses.map do |wr|
       if body.match?(Regexp.new(wr.match_value))
         wr.invoke_effect(self)
       end
     end.compact
     
-    "#{wechat_config.help_feedback}#{res.join(', ')}"
+    "#{wechat_app.help_feedback}#{res.join(', ')}"
   end
   
 end
