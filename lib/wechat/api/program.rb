fe7b7ed0c7d05c0085b71ffc6ad02a9dc7fb353b
qinmingyuan <mingyuan0715@foxmail.com>
2019-12-12 01:33:24 +0800

    fix api

--- lib/wechat/api/program.rb
+++ lib/wechat/api/program.rb
@@ -8,7 +8,7 @@ class Wechat::Api::Program < Wechat::Api::Base
   end
 
   # https://developers.weixin.qq.com/miniprogram/dev/api-backend/open-api/template-message/templateMessage.getTemplateList.html
-  def list_message_template(offset: 0, count: 20)
+  def list_message_template
     r = get 'newtmpl/gettemplate', base: WXAAPI
     r['data']
   end
