c81d789ffc39ae35775a8638f83832f4fbf1f5e4
qinmingyuan <mingyuan0715@foxmail.com>
2019-12-12 18:10:29 +0800

    has templates

--- app/models/rails_wechat/oauth_user/wechat_user.rb
+++ app/models/rails_wechat/oauth_user/wechat_user.rb
@@ -6,6 +6,7 @@ module RailsAuth::OauthUser::WechatUser
 
     has_many :wechat_requests, dependent: :delete_all
     has_many :ticket_items, dependent: :delete_all
+    has_many :wechat_subscribeds, dependent: :delete_all
     has_many :wechat_user_tags, dependent: :destroy
     has_many :wechat_tags, through: :wechat_user_tags
   end
