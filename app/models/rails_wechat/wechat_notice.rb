e8de13aa06835c05a978f8811c0011a034755520
qinmingyuan <mingyuan0715@foxmail.com>
2020-01-03 01:33:06 +0800

    message send logic

--- app/models/rails_wechat/wechat_notice.rb
+++ app/models/rails_wechat/wechat_notice.rb
@@ -2,16 +2,21 @@ module RailsWechat::WechatNotice
   extend ActiveSupport::Concern
 
   included do
-    
-    belongs_to :public_template
+
+    belongs_to :wechat_template
     belongs_to :wechat_app
     has_many :wechat_subscribeds
-    
+
     before_validation do
       self.wechat_app = wechat_template.wechat_app
     end
   end
 
-  
+
+  def to_wechat
+    x = Wechat::Message::Template::Program.new()
+    x.do_send
+  end
+
 
 end
--- lib/wechat/message/template/program.rb
+++ lib/wechat/message/template/program.rb
@@ -1,9 +1,10 @@
 class Wechat::Message::Template::Program < Wechat::Message::Template
-  
+
   def to(openid, **options)
     @message_hash.merge!(touser: openid, **options)
   end
-  
+
+  # https://developers.weixin.qq.com/miniprogram/dev/api-backend/open-api/subscribe-message/subscribeMessage.send.html
   def do_send
     api.post 'message/subscribe/send', {}, base: API_BASE
   end
