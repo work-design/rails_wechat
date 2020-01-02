c8bcc3375bd1027a9a8b10e65a6aafaf4517fe55
qinmingyuan <mingyuan0715@foxmail.com>
2020-01-02 22:47:14 +0800

    select notifiable type and code

--- app/models/rails_wechat/public_template.rb
+++ app/models/rails_wechat/public_template.rb
@@ -1,10 +1,14 @@
 module RailsWechat::PublicTemplate
   extend ActiveSupport::Concern
+
   included do
     attribute :title, :string
     attribute :tid, :string
     attribute :kid_list, :integer, array: true, default: []
     attribute :description, :string
+    attribute :notifiable_type, :string
+    attribute :code, :string, default: 'default'
+    attribute :mappings, :json, default: {}
   end
   
   def key_words
@@ -12,6 +16,10 @@ module RailsWechat::PublicTemplate
     return [] unless app
     app.api.template_key_words tid
   end
-  
+
+  def notify_setting
+    r = RailsNotice.notifiable_types.dig(notifiable_type, self.code.to_sym) || {}
+    r.fetch(:only, []).map(&->(o){ [notifiable_type.constantize.human_attribute_name(o), o] }).to_h
+  end
   
 end
--- app/models/rails_wechat/wechat_notice.rb
+++ app/models/rails_wechat/wechat_notice.rb
@@ -2,11 +2,8 @@ module RailsWechat::WechatNotice
   extend ActiveSupport::Concern
 
   included do
-    attribute :notifiable_type, :string
-    attribute :code, :string, default: 'default'
-    attribute :mappings, :json, default: {}
     
-    belongs_to :wechat_template
+    belongs_to :public_template
     belongs_to :wechat_app
     has_many :wechat_subscribeds
     
@@ -15,9 +12,6 @@ module RailsWechat::WechatNotice
     end
   end
 
-  def notify_setting
-    r = RailsNotice.notifiable_types.dig(notifiable_type, self.code.to_sym) || {}
-    r.fetch(:only, []).map(&->(o){ [notifiable_type.constantize.human_attribute_name(o), o] }).to_h
-  end
+  
 
 end
