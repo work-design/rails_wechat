9dfacb7ff7e3629cad3fd9fa08a51617a1155537
qinmingyuan <mingyuan0715@foxmail.com>
2020-01-25 23:01:50 +0800

    move method to subclass

--- app/models/rails_wechat/wechat_request.rb
+++ app/models/rails_wechat/wechat_request.rb
@@ -8,21 +8,12 @@ module RailsWechat::WechatRequest
     belongs_to :wechat_app
     has_many :extractions, as: :extractable  # 解析 request body 内容，主要针对文字
     has_many :ticket_items, dependent: :delete_all
-
-    after_save_commit :do_extract, if: -> { saved_change_to_body? }
   end
 
   def do_extract
-    wechat_app.extractors.map do |extractor|
-      matched = body.scan(extractor.scan_regexp)
-      next if matched.blank?
+  end
 
-      ex = self.extractions.find_or_initialize_by(extractor_id: extractor.id)
-      ex.name = extractor.name
-      ex.matched = matched.join(', ')
-      ex.save
-      ex
-    end
+  def response
   end
 
 end
--- app/models/rails_wechat/wechat_request/text_request.rb
+++ app/models/rails_wechat/wechat_request/text_request.rb
@@ -1,5 +1,9 @@
 module RailsWechat::WechatRequest::TextRequest
-  
+  extend ActiveSupport::Concern
+  included do
+    after_save_commit :do_extract, if: -> { saved_change_to_body? }
+  end
+
   def response
     res = "#{wechat_app.help}"
     return res unless body.match? Regexp.new(wechat_app.match_values)
@@ -8,8 +12,21 @@ module RailsWechat::WechatRequest::TextRequest
         wr.invoke_effect(self)
       end
     end.compact
-    
+
     "#{wechat_app.help_feedback}#{res.join(', ')}"
   end
-  
+
+  def do_extract
+    wechat_app.extractors.map do |extractor|
+      matched = body.scan(extractor.scan_regexp)
+      next if matched.blank?
+
+      ex = self.extractions.find_or_initialize_by(extractor_id: extractor.id)
+      ex.name = extractor.name
+      ex.matched = matched.join(', ')
+      ex.save
+      ex
+    end
+  end
+
 end
