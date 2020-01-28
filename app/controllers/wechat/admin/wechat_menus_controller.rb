58394221f5aa47027712e45a8622b37fbfe264bc
qinmingyuan <mingyuan0715@foxmail.com>
2020-01-28 17:03:40 +0800

    fix redirect to;

--- app/controllers/wechat/admin/wechat_menus_controller.rb
+++ app/controllers/wechat/admin/wechat_menus_controller.rb
@@ -2,20 +2,20 @@ class Wechat::Admin::WechatMenusController < Wechat::Admin::BaseController
   before_action :set_wechat_app
   before_action :set_wechat_menu, only: [:show, :edit, :edit_parent, :update, :destroy]
   before_action :prepare_form, only: [:new, :create, :edit, :update]
-  
+
   def default
     q_params = {}
     q_params.merge! wechat_app_id: nil
-  
+
     @wechat_menus = WechatMenu.where(q_params).order(parent_id: :desc, id: :asc).page(params[:page])
-    
+
     render 'index'
   end
-  
+
   def index
     q_params = {}
     q_params.merge! wechat_app_id: [params[:wechat_app_id], nil].uniq
-    
+
     @wechat_menus = WechatMenu.where(q_params).order(parent_id: :desc, id: :asc).page(params[:page])
   end
 
@@ -30,10 +30,10 @@ class Wechat::Admin::WechatMenusController < Wechat::Admin::BaseController
       render :new, locals: { model: @wechat_menu }, status: :unprocessable_entity
     end
   end
-  
+
   def sync
     r = @wechat_app.api.menu_create @wechat_app.menu
-    redirect_to admin_wechat_menus_url(wechat_app_id: @wechat_menu.wechat_app_id), notice: r.to_s
+    redirect_to admin_wechat_menus_url(wechat_app_id: @wechat_app.id), notice: r.to_s
   end
 
   def show
@@ -61,15 +61,15 @@ class Wechat::Admin::WechatMenusController < Wechat::Admin::BaseController
   def set_wechat_app
     @wechat_app = WechatApp.default_where(default_params).find_by id: params[:wechat_app_id]
   end
-  
+
   def set_wechat_menu
     @wechat_menu = WechatMenu.find(params[:id])
   end
-  
+
   def prepare_form
     @types = WechatMenu.options_i18n(:type)
     @types.reject! { |_, v| v == :ParentMenu }
-    
+
     @parents = WechatMenu.where(type: 'ParentMenu', parent_id: nil, wechat_app_id: params[:wechat_app_id])
   end
 
