e3f468cde6664a1ca567d4ac2d677aed69a3a2ec
qinmingyuan <mingyuan0715@foxmail.com>
2020-01-29 22:29:31 +0800

    fix order

--- app/models/rails_wechat/wechat_request.rb
+++ app/models/rails_wechat/wechat_request.rb
@@ -6,7 +6,7 @@ module RailsWechat::WechatRequest
 
     belongs_to :wechat_user
     belongs_to :wechat_app
-    has_many :extractions, as: :extractable, inverse_of: :wechat_request  # 解析 request body 内容，主要针对文字
+    has_many :extractions, -> { order(id: :asc) }, as: :extractable, inverse_of: :wechat_request  # 解析 request body 内容，主要针对文字
     has_many :ticket_items, dependent: :delete_all
   end
 
