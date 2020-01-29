7f4e437ec7b1bc3b14b953f05db8b28346943262
qinmingyuan <mingyuan0715@foxmail.com>
2020-01-29 22:35:42 +0800

    fix dependent

--- app/models/rails_wechat/wechat_request.rb
+++ app/models/rails_wechat/wechat_request.rb
@@ -6,8 +6,7 @@ module RailsWechat::WechatRequest
 
     belongs_to :wechat_user
     belongs_to :wechat_app
-    has_many :extractions, -> { order(id: :asc) }, as: :extractable, inverse_of: :wechat_request  # 解析 request body 内容，主要针对文字
-    has_many :ticket_items, dependent: :delete_all
+    has_many :extractions, -> { order(id: :asc) }, as: :extractable, inverse_of: :wechat_request, dependent: :delete_all  # 解析 request body 内容，主要针对文字
   end
 
   def do_extract
