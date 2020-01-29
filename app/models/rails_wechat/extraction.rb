fb381ca9660f3e1b00f02e84a01f009b02353b39
qinmingyuan <mingyuan0715@foxmail.com>
2020-01-29 23:13:22 +0800

    no more tickets

--- app/models/rails_wechat/extraction.rb
+++ app/models/rails_wechat/extraction.rb
@@ -8,11 +8,6 @@ module RailsWechat::Extraction
     belongs_to :wechat_request
     belongs_to :extractor
     belongs_to :extractable, polymorphic: true
-    belongs_to :wechat_user
-
-    before_validation do
-      self.wechat_user ||= wechat_request&.wechat_user
-    end
   end
 
   def respond_text
--- app/models/rails_wechat/oauth_user/wechat_user.rb
+++ app/models/rails_wechat/oauth_user/wechat_user.rb
@@ -6,7 +6,6 @@ module RailsWechat::OauthUser::WechatUser
     belongs_to :wechat_app, foreign_key: :app_id, primary_key: :appid
 
     has_many :wechat_requests, dependent: :delete_all
-    has_many :ticket_items, dependent: :delete_all
     has_many :wechat_subscribeds, dependent: :delete_all
     has_many :wechat_user_tags, dependent: :destroy
     has_many :wechat_tags, through: :wechat_user_tags
