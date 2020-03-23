9fb86472bd4ef6c307333bb273124a0348bab679
qinmingyuan <mingyuan0715@foxmail.com>
2020-03-23 20:13:13 +0800

    wechat

--- lib/wechat.rb
+++ lib/wechat.rb
@@ -4,10 +4,6 @@ require 'wechat/errors'
 require 'wechat/api'
 require 'wechat/helpers'
 require 'wechat/signature'
-
-module Wechat
-  autoload :Message, 'wechat/message'
-  autoload :Cipher, 'wechat/cipher'
-  autoload :ControllerApi, 'wechat/controller_api'
-
-end
+require 'wechat/cipher'
+require 'wechat/message'
+require 'wechat/controller_api'
