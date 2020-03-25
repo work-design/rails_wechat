8102ae5d5281dc66c5571252df056b36662f5e30
qinmingyuan <mingyuan0715@foxmail.com>
2020-03-25 19:08:15 +0800

    combine 2 same proc

--- app/models/concerns/wechat_request_extend.rb
+++ app/models/concerns/wechat_request_extend.rb
@@ -2,40 +2,35 @@ module WechatRequestExtend
   extend ActiveSupport::Concern
   included do
 
-    on msg_type: 'text', body: '绑定' do |received|
+    #msg_type: 'event', event: 'click', body: 'bind'
+    on msg_type: 'text', body: '绑定' do |request|
       reply_params = {
-        wechat_user_id: received.wechat_user.id,
+        wechat_user_id: request.wechat_user_id,
         news_reply_items_attributes: [
           {
             title: '请绑定',
             description: '绑定信息',
-            url: _routes.url_helpers.bind_my_oauth_users_url(uid: received.wechat_user.uid, organ_id: received.app.organ_id)
+            url: url_helpers.bind_my_oauth_users_url(uid: request.wechat_user.uid, organ_id: request.wechat_app.organ_id)
           }
         ]
       }
 
-      received.reply.with NewsReply.new(reply_params)
+      NewsReply.new(reply_params)
     end
 
-    on msg_type: 'event', event: 'templatesendjobfinish' do |received, content|
-      r = WechatNotice.find_by(msg_id: content['MsgID'])
-      r.update status: content['Status']
-      received.reply.with TextReply.new(wechat_user_id: received.wechat_user.id, value: 'SUCCESS')
+    on msg_type: 'event', event: 'templatesendjobfinish' do |request|
+      r = WechatNotice.find_by(msg_id: request.raw_body['MsgID'])
+      r.update status: request.raw_body['Status']
+
+      TextReply.new(wechat_user_id: request.wechat_user_id, value: 'SUCCESS')
     end
 
-    on msg_type: 'event', event: 'click', body: 'bind' do |received|
-      reply_parms = {
-        wechat_user_id: received.wechat_user.id,
-        news_reply_items_attributes: [
-          {
-            title: '请绑定',
-            description: '绑定信息',
-            url: _routes.url_helpers.bind_my_oauth_users_url(uid: received.wechat_user.uid)
-          }
-        ]
-      }
+  end
 
-      received.reply.with NewsReply.new(reply_parms)
+  class_methods do
+
+    def url_helpers
+      Rails.application.routes.url_helpers
     end
 
   end
