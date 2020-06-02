3fc9f4c688954a533d3388ac2e711936d8c5076e
qinmingyuan <mingyuan0715@foxmail.com>
2020-06-02 19:39:03 +0800

    wechat

--- app/models/rails_wechat/wechat_response.rb
+++ app/models/rails_wechat/wechat_response.rb
@@ -91,7 +91,7 @@ module RailsWechat::WechatResponse
       if matched.blank?
         next
       else
-        logger.debug "Matched: #{matched.inspect}"
+        logger.debug "Matched: #{matched.inspect}, Extractor: #{wechat_extractor.name}"
       end
 
       ex = request.wechat_extractions.find_or_initialize_by(wechat_extractor_id: wechat_extractor.id)
