7994dcba38d85c4b9cbcf617c2f108397f683a9d
qinmingyuan <mingyuan0715@foxmail.com>
2020-07-15 16:39:19 +0800

    wechat register status

--- app/models/rails_wechat/wechat_register.rb
+++ app/models/rails_wechat/wechat_register.rb
@@ -4,8 +4,15 @@ module RailsWechat::WechatRegister
   included do
     attribute :id_name, :string
     attribute :id_number, :string
+    attribute :state, :string, default: 'init'
 
     belongs_to :member
+
+    enum state: {
+      init: 'init',
+      doing: 'doing',
+      done: 'done'
+    }
   end
 
 end
