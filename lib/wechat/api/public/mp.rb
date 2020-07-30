8f017e23c28e4278c7d31e64647d33411bd67271
qinmingyuan <mingyuan0715@foxmail.com>
2020-07-30 12:45:17 +0800

    clean code

--- lib/wechat/api/public/mp.rb
+++ lib/wechat/api/public/mp.rb
@@ -2,7 +2,7 @@ module Wechat::Api::Public::Mp
   BASE = 'https://mp.weixin.qq.com/cgi-bin/'
 
   def qrcode(ticket)
-    client.get 'showqrcode', params: { ticket: ticket }, as: :file, base: BASE
+    get 'showqrcode', params: { ticket: ticket }, as: :file, base: BASE
   end
 
 end
