4877dae764913a6cebf2951a3d94112df9fc526c
qinmingyuan <mingyuan0715@foxmail.com>
2020-07-30 14:45:52 +0800

    program notice

--- app/models/rails_wechat/wechat_notice/program_notice.rb
+++ app/models/rails_wechat/wechat_notice/program_notice.rb
@@ -1,4 +1,5 @@
 module RailsWechat::WechatNotice::ProgramNotice
+  BASE = 'https://api.weixin.qq.com/cgi-bin/'
   extend ActiveSupport::Concern
 
   included do
@@ -6,13 +7,15 @@ module RailsWechat::WechatNotice::ProgramNotice
 
   # https://developers.weixin.qq.com/miniprogram/dev/api-backend/open-api/subscribe-message/subscribeMessage.send.html
   def do_send
-    api.post 'message/subscribe/send', @message_hash, base: API_BASE
+    wechat_app.api.post 'message/subscribe/send', **message_hash, base: BASE
   end
 
-  def to_message
-    @message_hash.merge! template_id: @notice.wechat_template.template_id
-    @message_hash.merge! touser: @notice.wechat_user.uid
-    @message_hash.merge! data: @notice.data
+  def message_hash
+    {
+      template_id: wechat_template.template_id,
+      touser: wechat_user.uid,
+      data: data
+    }
   end
 
 end
