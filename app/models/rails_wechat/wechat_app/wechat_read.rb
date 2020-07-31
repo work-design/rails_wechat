4c49c53d1485760b3c398d8222de92e3f0155c9c
qinmingyuan <mingyuan0715@foxmail.com>
2020-07-31 11:05:36 +0800

    as wechat read

--- app/models/rails_wechat/wechat_app/wechat_read.rb
+++ app/models/rails_wechat/wechat_app/wechat_read.rb
@@ -6,4 +6,17 @@ module RailsWechat::WechatApp::WechatRead
   end
 
 
+  def oauth2_url(scope = 'snsapi_userinfo', **host_options)
+    options = Rails.application.routes.default_url_options
+    h = {
+      appid: appid,
+      redirect_uri: url_helpers.wechat_app_url(id, **options),
+      response_type: 'code',
+      scope: scope,
+      state: SecureRandom.hex(16)
+    }
+    "https://open.weixin.qq.com/connect/oauth2/authorize?#{h.to_query}#wechat_redirect"
+  end
+
+
 end
