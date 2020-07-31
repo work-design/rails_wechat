160bc459ebf546a2489721a4c97b587f1315dc6d
qinmingyuan <mingyuan0715@foxmail.com>
2020-07-31 21:18:37 +0800

    not use before create

--- app/models/rails_wechat/wechat_template.rb
+++ app/models/rails_wechat/wechat_template.rb
@@ -13,7 +13,7 @@ module RailsWechat::WechatTemplate
     has_many :wechat_notices, dependent: :delete_all
 
     before_save :sync_from_template_config, if: -> { template_config_id_changed? || template_config }
-    before_create :sync_to_wechat
+    before_save :sync_to_wechat, if: -> { template_id_changed? && template_id.present? }
     after_destroy_commit :del_to_wechat
   end
 
@@ -31,15 +31,13 @@ module RailsWechat::WechatTemplate
   end
 
   def sync_to_wechat
-    if template_id.present?
-      sync_from_wechat
-    end
+    sync_from_wechat
     return if content.present?
     r = wechat_app.api.add_template(template_config.tid, template_config.kid_list)
-    logger.debug(r['errmsg'])
     if r['errcode'] == 0
-      self.template_id = r['priTmplId']
+      self.template_id = r['priTmplId'] || r['template_id']
     else
+      logger.debug("  =========> Error is #{r['errmsg']}")
       return
     end
     sync_from_wechat
@@ -50,6 +48,7 @@ module RailsWechat::WechatTemplate
       tid = wechat_app.is_a?(WechatPublic) ? i['template_id'] : i['priTmplId']
       tid == self.template_id
     end
+    return if r_content.blank?
     self.template_type = r_content['type']
     self.assign_attributes r_content.slice('title', 'content', 'example')
     self
