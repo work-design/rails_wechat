97a158ebbf5aa660dce9e4b765d791ddbd877664
qinmingyuan <mingyuan0715@foxmail.com>
2020-08-06 13:53:47 +0800

    has many wechat regitsers

--- app/models/rails_wechat/member.rb
+++ app/models/rails_wechat/member.rb
@@ -2,7 +2,7 @@ module RailsWechat::Member
   extend ActiveSupport::Concern
 
   included do
-    has_many :wechat_registers, dependent: :destroy
+    has_many :wechat_registers, foreign_key: :mobile, primary_key: :identity, dependent: :destroy
   end
 
 end
