6a5bfd4999d2522602eb3c9c2c4c382a3ee519d6
qinmingyuan <mingyuan0715@foxmail.com>
2020-08-11 10:55:27 +0800

    add do send

--- app/models/rails_wechat/wechat_service.rb
+++ app/models/rails_wechat/wechat_service.rb
@@ -12,13 +12,18 @@ module RailsWechat::WechatService
     belongs_to :wechat_request, optional: true
     belongs_to :wechat_app, foreign_key: :appid, primary_key: :appid, optional: true
     belongs_to :wechat_user, foreign_key: :open_id, primary_key: :uid, optional: true
+    belongs_to :wechat_agency, foreign_key: :appid, primary_key: :appid, optional: true
 
-    before_initialize if: :new_record? do
+    after_initialize if: :new_record? do
       if wechat_request
         self.appid = wechat_request.appid
         self.open_id = wechat_request.open_id
       end
     end
+  end
+
+  def do_send
+    wechat_agency.api.message_custom_send to_wechat
   end
 
   def content
