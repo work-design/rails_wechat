2b3779a4e596248549a395168a0f02bec5383cf8
qinmingyuan <mingyuan0715@foxmail.com>
2020-08-11 15:19:14 +0800

    test case

--- app/models/rails_wechat/wechat_auth.rb
+++ app/models/rails_wechat/wechat_auth.rb
@@ -9,7 +9,6 @@ module RailsWechat::WechatAuth
     belongs_to :wechat_platform
 
     after_create_commit :deal_auth_code
-    after_create_commit :deal_test_case, if: -> { testcase }
   end
 
   def deal_auth_code
@@ -17,11 +16,12 @@ module RailsWechat::WechatAuth
     return unless r
     agency = wechat_platform.wechat_agencies.find_or_initialize_by(appid: r['authorizer_appid'])
     agency.store_access_token(r)
+    deal_test_case(agency) if testcase
   end
 
   # 第三方平台上线测试用例2
   # 测试公众号使用客服消息接口处理用户消息
-  def deal_test_case
+  def deal_test_case(agency)
     text_service = agency.wechat_services.build(type: 'TextService')
     text_service.value = "#{auth_code}_from_api"
     text_service.save
