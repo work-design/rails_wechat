6baf89404ca0b559d2f65e82a528edd8769dad41
qinmingyuan <mingyuan0715@foxmail.com>
2020-08-14 13:25:52 +0800

    add order

--- app/models/rails_wechat/wechat_menu/parent_menu.rb
+++ app/models/rails_wechat/wechat_menu/parent_menu.rb
@@ -1,13 +1,14 @@
 module RailsWechat::WechatMenu::ParentMenu
   extend ActiveSupport::Concern
+
   included do
     has_many :children, class_name: self.base_class.name, foreign_key: :parent_id, dependent: :nullify
   end
-  
+
   def as_json
     {
       name: name,
-      sub_button: children.as_json
+      sub_button: children.order(position: :asc).as_json
     }
   end
 
