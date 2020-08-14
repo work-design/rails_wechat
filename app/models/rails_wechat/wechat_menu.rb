acc1948421627618d4cce390965efe2b0e801e41
qinmingyuan <mingyuan0715@foxmail.com>
2020-08-14 12:20:12 +0800

    position with scope

--- app/models/rails_wechat/wechat_menu.rb
+++ app/models/rails_wechat/wechat_menu.rb
@@ -2,8 +2,6 @@ module RailsWechat::WechatMenu
   extend ActiveSupport::Concern
 
   included do
-    acts_as_list
-
     attribute :type, :string
     attribute :menu_type, :string
     attribute :name, :string
@@ -16,6 +14,8 @@ module RailsWechat::WechatMenu
     belongs_to :wechat_app, foreign_key: :appid, primary_key: :appid, optional: true
     belongs_to :parent, class_name: self.base_class.name, optional: true
     has_many :children, class_name: self.base_class.name, foreign_key: :parent_id, dependent: :nullify
+
+    acts_as_list scope: [:parent_id, :appid]
   end
 
 
