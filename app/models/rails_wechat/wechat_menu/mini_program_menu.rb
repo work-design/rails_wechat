b714456875f337be01de939402ff89697f0af637
MingyuanQin <mingyuan0715@foxmail.com>
2020-08-28 00:38:12 +0800

    menu

--- app/models/rails_wechat/wechat_menu/mini_program_menu.rb
+++ app/models/rails_wechat/wechat_menu/mini_program_menu.rb
@@ -1,14 +1,17 @@
 module RailsWechat::WechatMenu::MiniProgramMenu
   extend ActiveSupport::Concern
+
   included do
     attribute :menu_type, :string, default: 'miniprogram'
   end
-  
+
   def as_json
     {
       type: menu_type,
       name: name,
-      key: value
+      url: value,
+      appid: mp_appid,
+      pagepath: mp_pagepath
     }
   end
 
