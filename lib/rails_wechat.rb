bb290c592f138f1fad43885b96abeadbcdfe52f0
qinmingyuan <mingyuan0715@foxmail.com>
2021-01-17 10:56:20 +0800

    enum with scope

--- lib/rails_wechat.rb
+++ lib/rails_wechat.rb
@@ -8,3 +8,11 @@ require 'rails_wechat/config'
 #require 'omniauth/strategies/wechat'
 #require 'omniauth/strategies/wechat_qiye'
 #require 'omniauth/strategies/wechat_qr'
+
+module Wechat
+
+  def self.use_relative_model_naming?
+    true
+  end
+
+end
