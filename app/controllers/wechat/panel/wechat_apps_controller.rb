838116905bc998f51bd5fc3874dc6b94215c941e
qinmingyuan <mingyuan0715@foxmail.com>
2021-03-20 12:25:32 +0800

    eff

--- app/controllers/wechat/panel/wechat_apps_controller.rb
+++ app/controllers/wechat/panel/wechat_apps_controller.rb
@@ -45,7 +45,7 @@ module Wechat
 
     private
     def set_wechat_app
-      @wechat_app = WechatApp.default_where(default_params).find(params[:id])
+      @wechat_app = WechatApp.find(params[:id])
     end
 
     def wechat_app_params
