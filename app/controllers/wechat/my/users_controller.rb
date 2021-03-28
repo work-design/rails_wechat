311d386a93c765270168251bca25c3e8018af822
qinmingyuan <mingyuan0715@foxmail.com>
2021-03-28 12:39:20 +0800

    sow

--- app/controllers/wechat/my/users_controller.rb
+++ app/controllers/wechat/my/users_controller.rb
@@ -2,8 +2,7 @@ module Wechat
   class My::UsersController < My::BaseController
 
     def invite_qrcode
-      scene = current_user.invite_scene(current_wechat_app)
-      @qrcode_url = scene.qrcode_file_url
+      @scene = current_user.invite_scene(current_wechat_app)
     end
 
   end
