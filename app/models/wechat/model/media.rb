6e29e560f7fc88536f1bfe35dd03386d505f219f
MingyuanQin <mingyuan0715@foxmail.com>
2021-04-01 21:50:59 +0800

    update

--- app/models/wechat/model/media.rb
+++ app/models/wechat/model/media.rb
@@ -7,7 +7,7 @@ module Wechat
       attribute :appid, :string
       attribute :attachment_name, :string
 
-      belongs_to :source, polymorphic: true, optional: true
+      belongs_to :source, polymorphic: true, optional: true, autosave: true
       belongs_to :app, foreign_key: :appid, primary_key: :appid
 
       belongs_to :user, class_name: 'Auth::User'
