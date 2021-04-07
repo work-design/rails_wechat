2564f2bef5906e5c26e495a1243dfa64ccfdaecb
qinmingyuan <mingyuan0715@foxmail.com>
2021-04-07 13:20:49 +0800

    requests

--- app/controllers/wechat/my/users_controller.rb
+++ app/controllers/wechat/my/users_controller.rb
@@ -7,6 +7,11 @@ module Wechat
 
     def requests
       @scene = current_user.invite_scene(current_wechat_app)
+      if @scene.tag
+        @requests =  @scene.tag.requests.page(params[:page]).per(3)
+      else
+        @requests = Request.none
+      end
     end
 
   end
