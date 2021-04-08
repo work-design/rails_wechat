8c6f33128a1e7ff7e028aff17f8954938a914651
qinmingyuan <mingyuan0715@foxmail.com>
2021-04-08 18:27:41 +0800

    reply

--- app/models/wechat/model/request.rb
+++ app/models/wechat/model/request.rb
@@ -100,7 +100,7 @@ module Wechat
 
       if reply.is_a?(SuccessReply)
         self.reply_body = {}
-      elsif reply
+      elsif reply.is_a? Reply
         self.reply_body = reply.to_wechat
         self.reply_body.merge!(ToUserName: open_id)
         self.reply_body.merge!(FromUserName: app.user_name) if app
