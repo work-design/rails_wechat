44ae0540ae7c0494cad161d29ed84dd422989a74
qinmingyuan <mingyuan0715@foxmail.com>
2021-04-10 00:49:56 +0800

    generate wechat user before get reply

--- app/models/wechat/model/receive.rb
+++ app/models/wechat/model/receive.rb
@@ -80,6 +80,7 @@ module Wechat
       build_request(type: compute_type)
       request.appid = appid
       request.open_id = open_id
+      request.generate_wechat_user
       request.msg_type = msg_type
       request.raw_body = message_hash.except('ToUserName', 'FromUserName', 'CreateTime', 'MsgType')
 
