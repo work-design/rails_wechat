03935d28e42c66846e1a2d5e1811460f7f1b11ca
qinmingyuan <mingyuan0715@foxmail.com>
2021-08-07 21:43:37 +0800

    dummy

--- Gemfile
+++ Gemfile
@@ -5,8 +5,7 @@ gemspec
 gem 'pg'
 gem 'puma'
 
-gem 'factory_bot_rails', require: false
-gem 'default_form', github: 'qinmingyuan/default_form'
+gem 'viter', github: 'qinmingyuan/viter'
 gem 'rails_com', github: 'work-design/rails_com'
 gem 'rails_ui', github: 'work-design/rails_ui'
 gem 'rails_auth', github: 'work-design/rails_auth'
@@ -17,5 +16,4 @@ gem 'acts_as_list'
 
 gem 'pry-rails'
 gem 'pry-stack_explorer'
-gem 'awesome_print'
 gem 'sdoc'
--- test/controllers/wechat/admin/extractors_controller_test.rb
+++ test/controllers/wechat/admin/extractors_controller_test.rb
@@ -1,48 +1,50 @@
 require 'test_helper'
-class Wechat::Admin::ExtractorsControllerTest < ActionDispatch::IntegrationTest
- 
-  setup do
-    @extractor = create :extractor
-  end
+module Wechat
+  class Admin::ExtractorsControllerTest < ActionDispatch::IntegrationTest
 
-  test 'index ok' do
-    get admin_extractors_url
-    assert_response :success
-  end
-
-  test 'new ok' do
-    get new_admin_extractor_url, xhr: true
-    assert_response :success
-  end
-
-  test 'create ok' do
-    assert_difference('Extractor.count') do
-      post admin_extractors_url, params: { extractor: { name: 'test' } }, xhr: true
+    setup do
+      @extractor = create :extractor
     end
 
-    assert_response :success
-  end
-
-  test 'show ok' do
-    get admin_extractor_url(@extractor), xhr: true
-    assert_response :success
-  end
-
-  test 'edit ok' do
-    get edit_admin_extractor_url(@extractor), xhr: true
-    assert_response :success
-  end
-
-  test 'update ok' do
-    patch admin_extractor_url(@extractor), params: { extractor: { name: 'test' } }, xhr: true
-    assert_response :success
-  end
-
-  test 'destroy ok' do
-    assert_difference('Extractor.count', -1) do
-      delete admin_extractor_url(@extractor), xhr: true
+    test 'index ok' do
+      get admin_extractors_url
+      assert_response :success
     end
 
-    assert_response :success
+    test 'new ok' do
+      get new_admin_extractor_url, xhr: true
+      assert_response :success
+    end
+
+    test 'create ok' do
+      assert_difference('Extractor.count') do
+        post admin_extractors_url, params: { extractor: { name: 'test' } }, xhr: true
+      end
+
+      assert_response :success
+    end
+
+    test 'show ok' do
+      get admin_extractor_url(@extractor), xhr: true
+      assert_response :success
+    end
+
+    test 'edit ok' do
+      get edit_admin_extractor_url(@extractor), xhr: true
+      assert_response :success
+    end
+
+    test 'update ok' do
+      patch admin_extractor_url(@extractor), params: { extractor: { name: 'test' } }, xhr: true
+      assert_response :success
+    end
+
+    test 'destroy ok' do
+      assert_difference('Extractor.count', -1) do
+        delete admin_extractor_url(@extractor), xhr: true
+      end
+
+      assert_response :success
+    end
   end
 end
