6f2e1ee9c61cdad795de8d02b655b43018e60e22
qinmingyuan <mingyuan0715@foxmail.com>
2021-10-01 16:54:56 +0800

    just use uid for relation

--- app/controllers/wechat/program_users_controller.rb
+++ app/controllers/wechat/program_users_controller.rb
@@ -9,7 +9,6 @@ module Wechat
       info = @app.api.jscode2session(session_params[:code])
       @program_user = ProgramUser.create_or_find_by!(uid: info['openid']) do |program_user|
         program_user.appid = params[:appid]
-        program_user.identity = info['openid']
         program_user.unionid = info['unionId']
       end
       auth_token = @program_user.auth_token(info['session_key'])
