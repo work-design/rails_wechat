b33f1f5284eca703d6c3ab4dfcac2a9e33e89f36
qinmingyuan <mingyuan0715@foxmail.com>
2021-10-03 00:47:30 +0800

    for secrue

--- app/models/wechat/model/request.rb
+++ app/models/wechat/model/request.rb
@@ -101,10 +101,12 @@ module Wechat
     def generate_wechat_user
       wechat_user || build_wechat_user
       wechat_user.appid = appid
-      if body.to_s.start_with? 'invite_by_'
-        wechat_user.user_inviter_id = body.delete_prefix('invite_by_')
-      elsif body.to_s.start_with? 'invite_member_'
-        wechat_user.member_inviter_id = body.delete_prefix('invite_member_')
+      if ['SCAN', 'subscribe'].include?(event)
+        if event_key.to_s.start_with?('invite_by_')
+          wechat_user.user_inviter_id = event_key.delete_prefix('invite_by_')
+        elsif event_key.to_s.start_with? 'invite_member_'
+          wechat_user.member_inviter_id = event_key.delete_prefix('invite_member_')
+        end
       end
 
       if wechat_user.new_record?
