322b608e22cb391387a48129c595a6231fd275e3
qinmingyuan <mingyuan0715@foxmail.com>
2021-10-02 12:05:47 +0800

    add member

--- app/models/wechat/ext/member.rb
+++ app/models/wechat/ext/member.rb
@@ -1,15 +1,15 @@
 module Wechat
-  module Ext::User
+  module Ext::Member
     extend ActiveSupport::Concern
 
     included do
-      has_many :wechat_users, class_name: 'Wechat::WechatUser', through: :accounts, source: :oauth_users
-      has_many :program_users, class_name: 'Wechat::ProgramUser', through: :accounts, source: :oauth_users
+      has_many :wechat_users, class_name: 'Wechat::WechatUser', through: :account, source: :oauth_users
+      has_many :program_users, class_name: 'Wechat::ProgramUser', through: :account, source: :oauth_users
       has_many :medias, class_name: 'Wechat::Media'
       has_many :subscribes, ->{ where(sending_at: nil).order(id: :asc) }, class_name: 'Wechat::Subscribe', through: :program_users
     end
 
-    def invite_scene(app, prefix = 'invite_by')
+    def invite_scene(app, prefix = 'invite_by_member')
       scene = Scene.find_or_initialize_by(appid: app.appid, match_value: "#{prefix}_#{id}")
       scene.expire_seconds ||= 2592000
       scene.organ_id = app.organ_id
