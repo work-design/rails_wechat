6f94bdf840ace050f2a0483ca84a7f63b869b1a9
qinmingyuan <mingyuan0715@foxmail.com>
2021-11-05 00:47:57 +0800

    protect nil

--- app/models/wechat/model/request/scan_request.rb
+++ app/models/wechat/model/request/scan_request.rb
@@ -9,7 +9,7 @@ module Wechat
 
     def login_user
       r = body.delete_prefix!('session_')
-      auth_token = wechat_user.authorized_tokens.valid.first || wechat_user.account.authorized_tokens.valid.first
+      auth_token = wechat_user.authorized_tokens.valid.first || wechat_user.account&.authorized_tokens&.valid&.first
       Com::SessionChannel.broadcast_to(r, auth_token: auth_token.token) if r && auth_token
     end
 
