75d1beaab913b01c817472537f5a9a0b1fc76e91
qinmingyuan <mingyuan0715@foxmail.com>
2021-11-05 01:10:36 +0800

    simple tags

--- app/models/wechat/model/request/scan_request.rb
+++ app/models/wechat/model/request/scan_request.rb
@@ -3,7 +3,7 @@ module Wechat
     extend ActiveSupport::Concern
 
     included do
-      before_save :sync_to_tag
+      before_save :sync_to_tag, unless: -> { body.to_s.start_with?('session_') }
       after_create_commit :login_user, if: -> { body.to_s.start_with?('session_') }
     end
 
--- app/models/wechat/model/request/subscribe_request.rb
+++ app/models/wechat/model/request/subscribe_request.rb
@@ -3,7 +3,7 @@ module Wechat
     extend ActiveSupport::Concern
 
     included do
-      before_save :sync_to_tag
+      before_save :sync_to_tag, unless: -> { body.to_s.start_with?('session_') }
     end
 
   end
