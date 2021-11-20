1d8d3d181caad4423dc8fff7593eca1a3a5fd6c1
qinmingyuan <mingyuan0715@foxmail.com>
2021-11-20 21:03:33 +0800

    data

--- app/apis/wechat/api/program/wxaapi.rb
+++ app/apis/wechat/api/program/wxaapi.rb
@@ -5,11 +5,11 @@ module Wechat::Api
     # https://developers.weixin.qq.com/miniprogram/dev/api-backend/open-api/template-message/templateMessage.getTemplateList.html
     def templates
       r = get 'newtmpl/gettemplate', base: BASE
-      if r['errcode'] === 0
+      if r['errcode'] == 0
         r['data']
       else
         Rails.logger.info r
-        []
+        r
       end
     end
 
@@ -20,8 +20,11 @@ module Wechat::Api
     # https://developers.weixin.qq.com/miniprogram/dev/api-backend/open-api/subscribe-message/subscribeMessage.getPubTemplateKeyWordsById.html
     def template_key_words(tid)
       r = get 'newtmpl/getpubtemplatekeywords', params: { tid: tid }, base: BASE
-      r['data']
-      r
+      if r['errcode'] == 0
+        r['data']
+      else
+        r
+      end
     end
 
     def add_template(tid, kid_list, description: 'tst')
