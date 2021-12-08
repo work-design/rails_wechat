22d455608e586f9057beca3739744083e388b19f
qinmingyuan <mingyuan0715@foxmail.com>
2021-12-08 22:51:24 +0800

    gemfile

--- app/apis/wechat/api/program/cgi_bin.rb
+++ app/apis/wechat/api/program/cgi_bin.rb
@@ -2,7 +2,7 @@ module Wechat::Api
   module Program::CgiBin
     BASE = 'https://api.weixin.qq.com/wxa/'
 
-    def create_wxa_qrcode(path, width = 430)
+    def create_wxa_qrcode(path = '/pages/index/index', width = 430)
       post 'wxaapp/createwxaqrcode', path: path, width: width, base: BASE
     end
 
