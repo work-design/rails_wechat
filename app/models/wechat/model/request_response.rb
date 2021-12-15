81b79a32dabba26c6ab521992b5ef9ee8d400581
qinmingyuan <mingyuan0715@foxmail.com>
2021-12-16 00:12:11 +0800

    response

--- app/models/wechat/model/request_response.rb
+++ app/models/wechat/model/request_response.rb
@@ -6,7 +6,7 @@ module Wechat
       attribute :request_type, :string
       attribute :appid, :string
 
-      belongs_to :respose
+      belongs_to :response
 
       belongs_to :request, ->(o){ where(appid: o.appid) }, foreign_key: :type, primary_key: :request_type
     end
