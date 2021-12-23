395e4bd6464ed8b9eef6184f6188376b0fb5c60d
qinmingyuan <mingyuan0715@foxmail.com>
2021-12-23 09:55:24 +0800

    request type

--- app/models/wechat/model/response.rb
+++ app/models/wechat/model/response.rb
@@ -14,7 +14,7 @@ module Wechat
       belongs_to :reply, optional: true
       has_many :extractors, dependent: :delete_all
       has_many :hooks, dependent: :delete_all
-      has_many :request_responses, dependent: :destroy_async
+      has_many :request_responses, ->(o){ where(appid: o.appid) }, dependent: :destroy_async
 
       validates :match_value, presence: true
 
