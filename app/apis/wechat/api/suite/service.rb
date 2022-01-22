c39b3d6793babc68335f35110760359a94d748cd
qinmingyuan <mingyuan0715@foxmail.com>
2022-01-22 16:47:45 +0800

    service

--- app/apis/wechat/api/suite/service.rb
+++ app/apis/wechat/api/suite/service.rb
@@ -3,7 +3,7 @@ module Wechat::Api
     BASE = 'https://qyapi.weixin.qq.com/cgi-bin/service/'
 
     def token
-      client.post 'get_suite_token', suite_id: app.suite_id, suite_secret: app.secret, suite_ticket: app.suite_ticket, base: BASE
+      client.post 'get_suite_token', { suite_id: app.suite_id, suite_secret: app.secret, suite_ticket: app.suite_ticket }.to_json, base: BASE
     end
 
   end
