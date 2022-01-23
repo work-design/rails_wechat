8124046e7e7c09473853e0156a3fe436d7c710a6
qinmingyuan <mingyuan0715@foxmail.com>
2022-01-23 11:35:17 +0800

    delete only suite ticket

--- app/jobs/wechat/provider_ticket_clean_job.rb
+++ app/jobs/wechat/provider_ticket_clean_job.rb
@@ -2,7 +2,7 @@ module Wechat
   class ProviderTicketCleanJob < ApplicationJob
 
     def perform(ticket)
-      ProviderTicket.where(suite_id: ticket.suite_id).where.not(id: ticket.id).delete_all
+      ProviderTicket.where(suite_id: ticket.suite_id, info_type: 'suite_ticket').where.not(id: ticket.id).delete_all
     end
 
   end
