437bbb87c82c8da3e7d6568c01b64a3ce9b2d20f
qinmingyuan <mingyuan0715@foxmail.com>
2022-01-24 21:51:19 +0800

    privider

--- app/models/wechat/model/provider_ticket.rb
+++ app/models/wechat/model/provider_ticket.rb
@@ -14,22 +14,25 @@ module Wechat
 
       belongs_to :provider, foreign_key: :suite_id, primary_key: :suite_id, optional: true
 
-      after_create :parsed_data, if: -> { provider.present? }
+      before_save :parsed_data
+      after_save :sync_suite_ticket, if: -> { ['suite_ticket'].include?(info_type) && saved_change_to_info_type? }
       after_save :sync_auth_code, if: -> { ['create_auth'].include?(info_type) && saved_change_to_info_type? }
       after_create_commit :clean_last
     end
 
     def parsed_data
       content = provider.decrypt(ticket_data)
-
       data = Hash.from_xml(content).fetch('xml', {})
+
+      self.info_type = data['InfoType']
+      self.message_hash = data
+      data
+    end
+
+    def sync_suite_ticket
       provider.suite_ticket_pre = provider.suite_ticket
       provider.suite_ticket = data['SuiteTicket']
       provider.save
-      self.info_type = data['InfoType']
-      self.message_hash = data
-      self.save
-      data
     end
 
     def sync_auth_code
