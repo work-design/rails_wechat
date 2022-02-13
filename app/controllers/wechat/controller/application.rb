72752f250924f9763d8f47a7af58972533ca10f2
qinmingyuan <mingyuan0715@foxmail.com>
2022-02-13 18:12:53 +0800

    corp user

--- app/controllers/wechat/controller/application.rb
+++ app/controllers/wechat/controller/application.rb
@@ -34,7 +34,7 @@ module Wechat
     def current_wechat_app
       return @current_wechat_app if defined?(@current_wechat_app)
       if request.user_agent =~ /wxwork/
-        @current_wechat_app = Provider.first
+        @current_wechat_app = current_account.corp_users[0]&.corp
       else
         @current_wechat_app = current_organ_domain&.wechat_app
       end
