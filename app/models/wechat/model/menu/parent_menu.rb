1bff998e51dd555ec9be07e92e885be912c48e55
qinmingyuan <mingyuan0715@foxmail.com>
2022-02-28 23:04:23 +0800

    moved unused code

--- app/models/wechat/model/menu/parent_menu.rb
+++ app/models/wechat/model/menu/parent_menu.rb
@@ -2,10 +2,6 @@ module Wechat
   module Model::Menu::ParentMenu
     extend ActiveSupport::Concern
 
-    included do
-      has_many :children, class_name: self.base_class.name, foreign_key: :parent_id, dependent: :nullify
-    end
-
     def as_json
       {
         name: name,
