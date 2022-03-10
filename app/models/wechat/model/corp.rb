c1a91c15bdf2dca7b7ed37e575b1eb9fee8c6fd5
qinmingyuan <mingyuan0715@foxmail.com>
2022-03-10 15:03:24 +0800

    suite

--- app/models/wechat/model/corp.rb
+++ app/models/wechat/model/corp.rb
@@ -78,7 +78,7 @@ module Wechat
     end
 
     def refresh_access_token
-      info = provider.api.corp_token(corp_id, permanent_code)
+      info = suite.api.corp_token(corp_id, permanent_code)
       self.access_token = info['access_token']
       self.access_token_expires_at = Time.current + info['expires_in'].to_i if info['access_token'] && self.access_token_changed?
       self.save
