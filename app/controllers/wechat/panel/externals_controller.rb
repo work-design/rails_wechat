4ed6bfdad7cc911e8872e7264aaceea4ab38bc61
qinmingyuan <mingyuan0715@foxmail.com>
2022-03-19 01:54:31 +0800

    corp directly

--- app/controllers/wechat/panel/externals_controller.rb
+++ app/controllers/wechat/panel/externals_controller.rb
@@ -3,9 +3,7 @@ module Wechat
     before_action :set_corp
 
     def index
-      q_params = {
-
-      }
+      q_params = {}
       q_params.merge! params.permit(:state, :userid)
 
       @externals = @corp.externals.default_where(q_params).page(params[:page])
@@ -13,7 +11,7 @@ module Wechat
 
     private
     def set_corp
-      @corp = Corp.find_by corp_id: params[:corp_id]
+      @corp = Corp.find params[:corp_id]
     end
   end
 end
