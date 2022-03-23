8a151e755df8139e13cf1cd20c1e744c4cca2f5d
qinmingyuan <mingyuan0715@foxmail.com>
2022-03-23 21:13:20 +0800

    crop user

--- app/models/wechat/model/follow.rb
+++ app/models/wechat/model/follow.rb
@@ -13,7 +13,7 @@ module Wechat
       attribute :external_userid, :string
 
       belongs_to :corp_user, foreign_key: :userid, primary_key: :user_id
-      belongs_to :external, foreign_key: :external_userid, primary_key: :external_userid
+      belongs_to :external, ->(o){ where(corp_id: o.corp_id) }, foreign_key: :external_userid, primary_key: :external_userid
 
       belongs_to :crm_tag, class_name: 'Crm::Tag', foreign_key: :state, primary_key: :name, optional: true
     end
