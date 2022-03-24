58916b91deef69ec0bbf3cc8456ac8f6f7d332b4
qinmingyuan <mingyuan0715@foxmail.com>
2022-03-24 22:12:49 +0800

    only works in work wechat

--- app/controllers/wechat/controller/application.rb
+++ app/controllers/wechat/controller/application.rb
@@ -66,7 +66,8 @@ module Wechat
 
     def current_corp_user
       return @current_corp_user if defined? @current_corp_user
-      @current_corp_user = current_authorized_token&.corp_user
+      @current_corp_user = current_authorized_token&.corp_user if request.variant.any?(:work_wechat)
+
       logger.debug "\e[35m  Login as Corp User: #{@current_corp_user&.id}  \e[0m"
       @current_corp_user
     end
