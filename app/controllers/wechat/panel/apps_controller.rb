64423ee7a399467c48327816b0c276a4d47727ce
qinmingyuan <mingyuan0715@foxmail.com>
2022-03-30 12:43:42 +0800

    apps

--- app/controllers/wechat/panel/apps_controller.rb
+++ app/controllers/wechat/panel/apps_controller.rb
@@ -4,7 +4,7 @@ module Wechat
 
     def index
       q_params = {}
-      q_params.merge! params.permit(:id, :type)
+      q_params.merge! params.permit(:id, :type, :appid)
 
       @apps = App.default_where(q_params).order(id: :asc).page(params[:page])
     end
