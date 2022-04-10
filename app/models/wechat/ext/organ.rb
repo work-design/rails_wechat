1fa0a1272d2c7f26b1a47f25f8e7db7bbf43a0bc
qinmingyuan <mingyuan0715@foxmail.com>
2022-04-10 17:27:22 +0800

    link to contact

--- app/models/wechat/ext/organ.rb
+++ app/models/wechat/ext/organ.rb
@@ -9,8 +9,16 @@ module Wechat
 
       has_many :apps, class_name: 'Wechat::App', dependent: :destroy_async
       has_many :corps, class_name: 'Wechat::Corp', primary_key: :corp_id, foreign_key: :corp_id
+      belongs_to :corp_user, class_name: 'Wechat::CorpUser', optional: true
 
       validates :limit_wechat_menu, inclusion: { in: [1, 2, 3] }
+    end
+
+    def contact
+      return @contact if defined? @contact
+      if corp_user
+        @contact ||= corp_user.contacts.find_or_create_by(state: '外部联系')
+      end
     end
 
   end
