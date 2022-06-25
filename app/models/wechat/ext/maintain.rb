10258b8dfad809e9c770f8adccdb3bb316dcfe76
qinmingyuan <mingyuan0715@foxmail.com>
2022-06-25 10:58:14 +0800

    external

--- app/models/wechat/ext/maintain.rb
+++ app/models/wechat/ext/maintain.rb
@@ -13,8 +13,8 @@ module Wechat
       attribute :corp_id, :string
       attribute :external_userid, :string
 
-      belongs_to :corp_user, ->(o){ where(corp_id: o.corp_id) }, foreign_key: :userid, primary_key: :user_id, optional: true
-      belongs_to :external, ->(o){ where(corp_id: o.corp_id) }, foreign_key: :external_userid, primary_key: :external_userid
+      belongs_to :corp_user, ->(o){ where(corp_id: o.corp_id) }, class_name: 'Wechat::CorpUser', foreign_key: :userid, primary_key: :user_id, optional: true
+      belongs_to :external, ->(o){ where(corp_id: o.corp_id) }, class_name: 'Wechat::External', foreign_key: :external_userid, primary_key: :external_userid
 
       belongs_to :crm_tag, class_name: 'Crm::Tag', foreign_key: :state, primary_key: :name, optional: true
     end
