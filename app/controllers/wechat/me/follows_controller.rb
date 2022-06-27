074ef65d8019f3a575c0402fede45a43bf297a86
qinmingyuan <mingyuan0715@foxmail.com>
2022-06-27 10:12:15 +0800

    fix actions

--- app/controllers/wechat/me/follows_controller.rb
+++ app/controllers/wechat/me/follows_controller.rb
@@ -1,11 +1,17 @@
 module Wechat
   class Me::FollowsController < Me::BaseController
+    before_action :set_follow, only: [:show, :edit, :update, :destroy, :actions]
 
     def index
       q_params = {}
       q_params.merge! params.permit(:state)
 
       @follows = current_corp_user.follows.includes(:client).default_where(q_params).page(params[:page])
+    end
+
+    private
+    def set_follow
+      @follow = current_corp_user.follows.find params[:id]
     end
 
   end
