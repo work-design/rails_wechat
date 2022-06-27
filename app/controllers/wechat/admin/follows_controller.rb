aefd47a816ad83932fca1f2136888b07d47c6d20
qinmingyuan <mingyuan0715@foxmail.com>
2022-06-27 19:04:55 +0800

    add follows

---
+++ app/controllers/wechat/admin/follows_controller.rb
@@ -0,0 +1,17 @@
+module Wechat
+  class Admin::FollowsController < Admin::BaseController
+    before_action :set_corp_user
+
+    def index
+      q_params = {}
+      q_params.merge! params.permit(:state, :userid)
+
+      @follows = @corp_user.follows.includes(:client).page(params[:page])
+    end
+
+    private
+    def set_corp_user
+      @corp_user = CorpUser.find params[:corp_user_id]
+    end
+  end
+end
