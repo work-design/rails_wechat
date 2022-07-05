d56c873d849b42d4e268823b7a6cccc49c26ae14
qinmingyuan <mingyuan0715@foxmail.com>
2022-07-05 19:17:38 +0800

    no more factory bot

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,13 +2,10 @@ ENV['RAILS_ENV'] = 'test'
 require_relative 'dummy/config/environment'
 require 'rails/test_help'
 require 'minitest/mock'
-require 'factory_bot'
 
 Minitest.backtrace_filter = Minitest::BacktraceFilter.new
-FactoryBot.find_definitions
 
 class ActiveSupport::TestCase
-  include FactoryBot::Syntax::Methods
   self.file_fixture_path = File.expand_path('fixtures/files', __dir__)
   parallelize(workers: 2)
 end
