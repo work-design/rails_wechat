55b73a0fc6fd12752702de4d79a5f7426f8964dc
qinmingyuan <mingyuan0715@foxmail.com>
2022-07-05 15:18:38 +0800

    Revert "use redirect to"
    This reverts commit 7cfafb33eb838ba4d7b3e9ea32e69fabafa5b839.

--- app/controllers/wechat/controller/application.rb
+++ app/controllers/wechat/controller/application.rb
@@ -25,7 +25,11 @@ module Wechat
         logger.debug "\e[35m  Redirect to: #{redirect_url}  \e[0m"
       end
 
-      redirect_to redirect_url, allow_other_host: true
+      if request.format.html?
+        render 'require_login', locals: { url: redirect_url }, layout: 'raw', status: 401
+      else
+        render 'require_login', locals: { url: redirect_url }, status: 401
+      end
     end
 
     def current_oauth_app
