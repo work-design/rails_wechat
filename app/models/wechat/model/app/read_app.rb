0be332533f73d5fa4bb226ce9dfd645a14ea1805
qinmingyuan <mingyuan0715@foxmail.com>
2022-07-18 11:53:26 +0800

    read app should not supprt oauth 2 url

--- app/models/wechat/model/app/read_app.rb
+++ app/models/wechat/model/app/read_app.rb
@@ -3,22 +3,7 @@ module Wechat
     extend ActiveSupport::Concern
 
     included do
-
     end
-
-
-    def oauth2_url(scope = 'snsapi_userinfo', **host_options)
-      options = Rails.application.routes.default_url_options
-      h = {
-        appid: appid,
-        redirect_uri: url_helpers.app_url(id, **options),
-        response_type: 'code',
-        scope: scope,
-        state: SecureRandom.hex(16)
-      }
-      "https://open.weixin.qq.com/connect/oauth2/authorize?#{h.to_query}#wechat_redirect"
-    end
-
 
   end
 end
