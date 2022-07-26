edc0fa62644f23b181d7f7fb11307642dd6a17c1
qinmingyuan <mingyuan0715@foxmail.com>
2022-07-26 11:36:38 +0800

    sept for debug

--- app/models/wechat/model/scene.rb
+++ app/models/wechat/model/scene.rb
@@ -75,11 +75,14 @@ module Wechat
       self.qrcode_url = r['url']
     end
 
-    def get_program_qrcode
-      query = {
+    def program_query
+      {
         org_id: "org_#{organ_id}",
         path: "#{match_value.delete_prefix('/')}"
       }
+    end
+
+    def get_program_qrcode
       if expire_seconds
         self.expire_at = Time.current + expire_seconds
         expire = { is_expire: true, expire_type: 0, expire_time: expire_at.to_i }
@@ -87,7 +90,7 @@ module Wechat
         expire = { is_expire: false }
       end
 
-      r = app.api.generate_url(query: query.to_query, **expire)
+      r = app.api.generate_url(query: program_query.to_query, **expire)
       self.qrcode_url = r['url_link']
       r
     end
