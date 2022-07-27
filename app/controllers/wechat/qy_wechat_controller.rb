6a55bd28af3a27cbcbbb2655c22a986f6ea2646b
qinmingyuan <mingyuan0715@foxmail.com>
2022-07-27 17:00:55 +0800

    try use the qy

--- app/controllers/wechat/qy_wechat_controller.rb
+++ app/controllers/wechat/qy_wechat_controller.rb
@@ -2,7 +2,7 @@ module Wechat
   class QyWechatController < BaseController
 
     def login
-      @app = WorkApp.default_where(default_params).take
+      @app = WorkApp.default_where(default_params).take || WorkApp.global.take
     end
 
   end
