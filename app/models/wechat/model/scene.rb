d8c2b7badd0e3b7add84be7cc2c0e2751e55b248
qinmingyuan <mingyuan0715@foxmail.com>
2022-08-07 00:29:09 +0800

    fix includes

--- app/models/wechat/model/scene.rb
+++ app/models/wechat/model/scene.rb
@@ -14,8 +14,8 @@ module Wechat
       belongs_to :organ, class_name: 'Org::Organ', optional: true
 
       belongs_to :app, foreign_key: :appid, primary_key: :appid
-      belongs_to :response, ->(o){ where(match_value: o.match_value) }, foreign_key: :appid, primary_key: :appid
-      belongs_to :tag, ->(o){ where(name: o.match_value) }, foreign_key: :appid, primary_key: :appid
+      belongs_to :response, ->(o){ where(appid: o.appid) }, foreign_key: :match_value, primary_key: :match_value
+      belongs_to :tag, ->(o){ where(appid: o.appid) }, foreign_key: :match_value, primary_key: :name
 
       has_many :app_menus, ->(o){ where(appid: o.appid) }, dependent: :destroy_async
       has_many :menus, -> { roots }, through: :app_menus
