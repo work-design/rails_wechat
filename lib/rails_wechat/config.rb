03464182a544e873987e2554628022601d70536d
qinmingyuan <mingyuan0715@foxmail.com>
2022-08-12 11:27:36 +0800

    simple for

--- lib/rails_wechat/config.rb
+++ lib/rails_wechat/config.rb
@@ -25,24 +25,6 @@ module RailsWechat
     config.rules.b = { msg_type: 'event', event: 'click', body: 'bind', proc: bind_proc }
     config.rules.b1 = { msg_type: 'event', event: ['subscribe', 'scan'], body: /^invite_by_/, proc: bind_proc }
     config.rules.b2 = { msg_type: 'event', event: ['subscribe', 'scan'], body: /^invite_member_/, proc: bind_proc }
-    config.rules.b3 = { msg_type: 'event', event: ['subscribe', 'scan'], proc: bind_proc }
-    config.rules.c = { msg_type: 'text', body: '绑定', proc: bind_proc }
-    config.rules.c0 = {
-      msg_type: 'text',
-      body: '退出',
-      proc: ->(request) {
-        Wechat::NewsReply.new(
-          appid: request.appid,
-          news_reply_items_attributes: [
-            {
-              title: '退出登录',
-              description: '绑定信息',
-              url: Rails.application.routes.url_for(controller: 'auth/sign', action: 'logout')
-            }
-          ]
-        )
-      }
-    }
     config.rules.c1 = {
       msg_type: 'text',
       body: 'TESTCOMPONENT_MSG_TYPE_TEXT',
