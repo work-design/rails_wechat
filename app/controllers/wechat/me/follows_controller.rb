1eee7cd517c39e146c8c785581ddcc042cbad976
qinmingyuan <mingyuan0715@foxmail.com>
2022-08-18 09:24:31 +0800

    curent maintains

--- app/controllers/wechat/me/follows_controller.rb
+++ app/controllers/wechat/me/follows_controller.rb
@@ -6,12 +6,12 @@ module Wechat
       q_params = {}
       q_params.merge! params.permit(:state, 'remark-like')
 
-      @follows = current_corp_user.follows.includes(:client).default_where(q_params).order(id: :desc).page(params[:page])
+      @follows = current_corp_user.maintains.includes(:client).default_where(q_params).order(id: :desc).page(params[:page])
     end
 
     private
     def set_follow
-      @follow = current_corp_user.follows.find params[:id]
+      @follow = current_corp_user.maintains.find params[:id]
     end
 
   end
