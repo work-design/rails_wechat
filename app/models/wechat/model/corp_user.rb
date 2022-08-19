4580f8fa00f936abac0ea448447f1e8e17eb8b05
qinmingyuan <mingyuan0715@foxmail.com>
2022-08-19 11:00:27 +0800

    if new recrod and user id preceent

--- app/models/wechat/model/corp_user.rb
+++ app/models/wechat/model/corp_user.rb
@@ -42,7 +42,7 @@ module Wechat
 
       validates :identity, presence: true
 
-      after_initialize :sync_identity, if: :new_record?
+      after_initialize :sync_identity, if: -> { new_record? && user_id.present? }
       before_validation :sync_identity, if: -> { user_id_changed? }
       before_validation :init_account, if: -> { identity_changed? }
       before_validation :init_corp, if: -> { suite_id.present? && suite_id_changed? }
