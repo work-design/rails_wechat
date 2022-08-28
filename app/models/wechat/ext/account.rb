d5647b2faf1fd0f4014b2d9037640956861c4b03
qinmingyuan <mingyuan0715@foxmail.com>
2022-08-29 00:27:44 +0800

    assign temp account to account

--- app/models/wechat/ext/account.rb
+++ app/models/wechat/ext/account.rb
@@ -3,7 +3,7 @@ module Wechat
     extend ActiveSupport::Concern
 
     included do
-      has_many :corp_users, class_name: 'Wechat::CorpUser', primary_key: :identity, foreign_key: :identity
+      has_many :corp_users, class_name: 'Wechat::CorpUser', primary_key: :identity, foreign_key: :identity, inverse_of: :account
       has_many :wechat_users, class_name: 'Wechat::WechatUser', primary_key: :identity, foreign_key: :identity
       #has_many :externals, class_name: 'Wechat::External', through: :wechat_users
     end
