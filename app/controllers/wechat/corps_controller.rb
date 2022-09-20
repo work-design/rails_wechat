792f8e1d7b8161107951385881a2db5fff6bd6a9
qinmingyuan <mingyuan0715@foxmail.com>
2022-09-20 15:04:14 +0800

    corp tickets

--- app/controllers/wechat/corps_controller.rb
+++ app/controllers/wechat/corps_controller.rb
@@ -1,9 +1,22 @@
 module Wechat
   class CorpsController < BaseController
-    before_action :set_corp, only: [:show, :verify]
+    before_action :set_corp, only: [:notify, :verify]
 
-    def show
-      render plain: 'success'
+    # 指令回调URL: /wechat/corps/:id/notify
+    def notify
+      @corp_ticket = @corp.corp_tickets.build
+      r = Hash.from_xml(request.raw_post)['xml']
+      logger.debug "\e[35m  body is: #{r}  \e[0m"
+
+      @corp_ticket.suiteid = r['ToUserName']
+      @corp_ticket.ticket_data = r['Encrypt']
+      @corp_ticket.agent_id = r['AgentID']
+
+      if @corp_ticket.save
+        render plain: 'success'
+      else
+        head :no_content
+      end
     end
 
     # 消息与事件接收URL: /wechat/corps/:id/callback
