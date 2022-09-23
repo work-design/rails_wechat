db3667464562a0f83f38ca6b8bb9bb7fa726ca0c
qinmingyuan <mingyuan0715@foxmail.com>
2022-09-23 17:29:25 +0800

    Revert "data"
    This reverts commit 16f7da49ab57ade6c5eb39cb026aad90cb52aadc.

--- app/apis/wechat/http_client.rb
+++ app/apis/wechat/http_client.rb
@@ -75,11 +75,29 @@ module Wechat
         data.binmode
         data.write(body)
         data.rewind
-        data
+        return data
       elsif content_type =~ /html|xml/
-        Hash.from_xml(body)
+        data = Hash.from_xml(body)
       else
-        JSON.parse body.gsub(/[\u0000-\u001f]+/, '')
+        data = JSON.parse body.gsub(/[\u0000-\u001f]+/, '')
+      end
+
+      case data['errcode']
+      when 0 # for request didn't expect results
+        data
+      # 42001: access_token timeout
+      # 40014: invalid access_token
+      # 40001, invalid credential, access_token is invalid or not latest hint
+      # 48001, api unauthorized hint, should not handle here # GH-230
+      # 40082, 企业微信
+      when 42001, 40014, 40001, 41001, 40082
+        raise Wechat::AccessTokenExpiredError
+        # 40029, invalid code for mp # GH-225
+        # 43004, require subscribe hint # GH-214
+      when 2
+        raise Wechat::ResponseError.new(data['errcode'], data['errmsg'])
+      else
+        data
       end
     end
 
