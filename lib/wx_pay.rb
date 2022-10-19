0827ffa4eb9201656081811926185e99093ed670
qinmingyuan <mingyuan0715@foxmail.com>
2022-10-19 11:02:20 +0800

    wxpay

--- lib/wx_pay.rb
+++ lib/wx_pay.rb
@@ -16,12 +16,10 @@ module WxPay
     config.pid = nil
     config.appid = nil
     config.target_id = nil
-    config.oauth_url = 'https://openauth.alipay.com/oauth2/appToAppAuth.htm'
     config.oauth_callback = nil
     config.return_url = nil
     config.notify_url = nil
     config.return_rsa = ''
-    config.rsa2_path = 'config/alipay_rsa2.pem'
   end
 
 end
