c4ceba79efda7faa94fc9baa7e88883071ac106f
qinmingyuan <mingyuan0715@foxmail.com>
2022-11-05 18:03:51 +0800

    ge

--- app/controllers/wechat/panel/providers_controller.rb
+++ app/controllers/wechat/panel/providers_controller.rb
@@ -1,16 +1,15 @@
 module Wechat
   class Panel::ProvidersController < Panel::BaseController
 
-
     private
     def provider_params
-      p = params.fetch(:provider, {}).permit(
+      _p = params.fetch(:provider, {}).permit(
         :name,
         :corp_id,
         :provider_secret
       )
-      p[:provider][:provider_secret].strip!
-      p
+      _p[:provider_secret]&.strip!
+      _p
     end
 
   end
