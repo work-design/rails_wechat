b406efe09f5733f1ed281efe453bcef0167d7c0a
qinmingyuan <mingyuan0715@foxmail.com>
2022-11-05 11:46:12 +0800

    clear last

--- app/jobs/wechat/suite_ticket_clean_job.rb
+++ app/jobs/wechat/suite_ticket_clean_job.rb
@@ -2,7 +2,7 @@ module Wechat
   class SuiteTicketCleanJob < ApplicationJob
 
     def perform(ticket)
-      SuiteTicket.where(suite_id: ticket.suite_id, info_type: 'suite_ticket').where.not(id: ticket.id).delete_all
+      SuiteTicket.where(suite_id: ticket.suite_id, info_type: ticket.info_type).where.not(id: ticket.id).delete_all
     end
 
   end
