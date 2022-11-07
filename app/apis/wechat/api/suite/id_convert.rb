328294afa16665041f2048a58c031267c23c4f6d
qinmingyuan <mingyuan0715@foxmail.com>
2022-11-07 18:53:28 +0800

    it is batch

--- app/apis/wechat/api/suite/id_convert.rb
+++ app/apis/wechat/api/suite/id_convert.rb
@@ -3,8 +3,8 @@ module Wechat::Api
     BASE = 'https://qyapi.weixin.qq.com/cgi-bin/idconvert/'
 
 
-    def to_pending_id(external_userid, **options)
-      raw_post 'external_userid_to_pending_id', external_userid: external_userid, base: BASE, **options
+    def to_pending_id(*external_userid, **options)
+      raw_post 'batch/external_userid_to_pending_id', external_userid: external_userid, base: BASE, **options
     end
   end
 end
