484dc99a0e287655f6029ad424f119ac7b23dd82
qinmingyuan <mingyuan0715@foxmail.com>
2022-11-08 14:40:26 +0800

    get external userid

--- app/apis/wechat/api/work/id_convert.rb
+++ app/apis/wechat/api/work/id_convert.rb
@@ -2,9 +2,13 @@ module Wechat::Api
   module Work::IdConvert
     BASE = 'https://qyapi.weixin.qq.com/cgi-bin/idconvert/'
 
-    def to_pending_id(*external_userid, **options)
+    def pending_id(*external_userid, **options)
       post 'batch/external_userid_to_pending_id', external_userid: external_userid, origin: BASE, **options
     end
+
+    def external_userid(unionid:, openid:, **options)
+      post 'unionid_to_external_userid', unionid: unionid, openid: openid, origin: BASE, **options
+    end
+
   end
 end
-
