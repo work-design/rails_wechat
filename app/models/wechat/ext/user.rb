8dd190ad7c4402205491978dd6f091bb608be320
qinmingyuan <mingyuan0715@foxmail.com>
2022-11-10 16:29:52 +0800

    show table

--- app/models/wechat/ext/user.rb
+++ app/models/wechat/ext/user.rb
@@ -5,7 +5,9 @@ module Wechat
 
     included do
       has_many :wechat_users, class_name: 'Wechat::WechatUser', through: :accounts, source: :oauth_users
+      has_many :user_inviters, through: :wechat_users
       has_many :program_users, class_name: 'Wechat::ProgramUser', through: :accounts, source: :oauth_users
+
       has_many :medias, class_name: 'Wechat::Media'
       has_many :subscribes, ->{ where(sending_at: nil).order(id: :asc) }, class_name: 'Wechat::Subscribe', through: :program_users
     end
