88780dc3ee027df059327e6b9216b423d59e4b7d
qinmingyuan <mingyuan0715@foxmail.com>
2022-11-18 09:22:20 +0800

    fix platformlogic

--- app/controllers/wechat/platforms_controller.rb
+++ app/controllers/wechat/platforms_controller.rb
@@ -1,7 +1,7 @@
 module Wechat
   class PlatformsController < BaseController
     skip_before_action :verify_authenticity_token, raise: false if whether_filter(:verify_authenticity_token)
-    before_action :set_platform, only: [:show, :message, :callback]
+    before_action :set_platform, only: [:show, :notify, :message, :callback]
 
     def show
     end
