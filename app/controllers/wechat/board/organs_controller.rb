79ec118920b0068ef095d8cd34e3ee624c4505d5
qinmingyuan <mingyuan0715@foxmail.com>
2022-12-28 19:25:07 +0800

    org organs admin

--- app/controllers/wechat/board/organs_controller.rb
+++ app/controllers/wechat/board/organs_controller.rb
@@ -1,53 +1,39 @@
 module Wechat
-  class Board::OrgansController < Wechat::BaseController
+  class Board::OrgansController < Org::Board::OrgansController
     before_action :set_organ, only: [:show, :edit, :update, :destroy]
+    before_action :set_corp, only: [:new, :index]
     before_action :set_new_organ, only: [:new, :create]
 
     def index
       @organs = current_account.organs.includes(:organ_domains)
 
       if @organs.blank?
-        @member = current_account.members.build
-        @organ = @member.build_organ
+        set_new_organ
         render :new
       else
         render 'index'
       end
     end
 
-    def new
-      @organ.who_roles.build(role_id: params[:role_id]) if params[:role_id].present?
-    end
-
-    def create
-      @member = @organ.members.build(owned: true)
-      @member.account = current_account
-    end
-
     private
+    def set_corp
+      @corp = Corp.find params[:corp_id] if params[:corp_id]
+    end
+
     def set_organ
       @organ = current_user.organs.find(params[:id])
     end
 
     def set_new_organ
-      @organ = Organ.new(organ_params)
-    end
-
-    def member_params
-      p = params.fetch(:member, {}).permit(
-        :identity
-      )
-      p.merge! owned: true
-      unless p[:identity]
-        p.merge! identity: current_account.identity
-      end
-      p
+      @member = current_account.members.build(owned: true)
+      @organ = @member.build_organ(corpid: @corp&.corp_id, **organ_params)
     end
 
     def organ_params
       params.fetch(:organ, {}).permit(
         :name,
         :logo,
+        :corpid,
         who_roles_attributes: {}
       )
     end
