18a585a50cb2f4f1626b98ddbc6d784b270e1b4c
qinmingyuan <mingyuan0715@foxmail.com>
2023-01-10 23:24:19 +0800

    simple

--- app/controllers/wechat/base_controller.rb
+++ app/controllers/wechat/base_controller.rb
@@ -6,19 +6,12 @@ module Wechat
     def xxx(oauth_user)
       state_hash = urlsafe_decode64(params[:state])
 
-      if oauth_user.user
-        login_by_oauth_user(oauth_user)
-        Com::SessionChannel.broadcast_to(params[:state], auth_token: current_authorized_token.id)
+      oauth_user.generate_account! unless oauth_user.user
+      login_by_oauth_user(oauth_user)
 
-        url = url_for(disposable_token: oauth_user.auth_token, **state_hash)
-        redirect_to url, allow_other_host: true
-      else
-        url_options = { host: state_hash[:host] }
-        url_options.merge! params.except(:controller, :action, :id, :business, :namespace, :code, :state).permit!
-        url = url_for(controller: 'auth/sign', action: 'bind', uid: oauth_user.uid, **url_options)
-
-        redirect_to url, allow_other_host: true
-      end
+      Com::SessionChannel.broadcast_to(params[:state], auth_token: current_authorized_token.id)
+      url = url_for(disposable_token: oauth_user.auth_token, **state_hash)
+      redirect_to url, allow_other_host: true
     end
 
   end
