c755af9b2f5e5df24e7622e890871d70a78b621e
qinmingyuan <mingyuan0715@foxmail.com>
2023-01-11 16:19:47 +0800

    logout

--- app/models/wechat/model/request/scan_request.rb
+++ app/models/wechat/model/request/scan_request.rb
@@ -13,6 +13,13 @@ module Wechat
       self.body = self.event_key
     end
 
+    def reply_from_response
+      r = super
+      return r if r.present?
+
+      Wechat::TextReply.new(value: '登录成功！') if body.to_s.start_with?('session_')
+    end
+
     def login_user
       session_str, url = body.split('@')
       session = session_str.delete_prefix!('session_')
