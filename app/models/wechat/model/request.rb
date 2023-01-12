25ea9c394c40609967c2151ea3a99c360abec8d4
qinmingyuan <mingyuan0715@foxmail.com>
2023-01-12 22:26:32 +0800

    d

--- app/models/wechat/model/request.rb
+++ app/models/wechat/model/request.rb
@@ -84,7 +84,7 @@ module Wechat
     def reply_for_blank_info
       return if wechat_user.attributes['name'].present? || !app.oauth_enable
       if agency
-        url = app.oauth2_url(host: agency.platform.domain, state: agency.base64_state(uid: open_id))
+        url = app.oauth2_url(host: agency.platform.domain, state: app.base64_state(uid: open_id))
       else
         url = app.oauth2_url(state: app.base64_state(uid: open_id))
       end
