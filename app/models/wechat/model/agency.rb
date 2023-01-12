ec003234147c3259ddcf85ea96050055181fe194
qinmingyuan <mingyuan0715@foxmail.com>
2023-01-12 22:34:56 +0800

    redirect to app id

--- app/models/wechat/model/agency.rb
+++ app/models/wechat/model/agency.rb
@@ -58,7 +58,7 @@ module Wechat
     end
 
     def oauth2_url(scope: 'snsapi_base', state: SecureRandom.hex(16), **url_options)
-      url_options.with_defaults! controller: 'wechat/apps', action: 'login', id: id, host: platform.domain
+      url_options.with_defaults! controller: 'wechat/apps', action: 'login', id: app.id, host: platform.domain
       h = {
         appid: appid,
         redirect_uri: Rails.application.routes.url_for(**url_options),
