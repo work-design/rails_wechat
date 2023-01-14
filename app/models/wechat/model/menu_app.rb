c88371abad8c2193fc78db07d858a8a477fb6db8
qinmingyuan <mingyuan0715@foxmail.com>
2023-01-14 12:17:07 +0800

    menu

--- app/models/wechat/model/menu_app.rb
+++ app/models/wechat/model/menu_app.rb
@@ -3,13 +3,14 @@ module Wechat
     extend ActiveSupport::Concern
 
     included do
-      attribute :appid, :string, index: true
       attribute :name, :string
+      attribute :appid, :string, index: true
 
+      belongs_to :menu
       belongs_to :app, foreign_key: :appid, primary_key: :appid, optional: true
+
       belongs_to :scene, optional: true
       belongs_to :tag, optional: true
-      belongs_to :menu
     end
 
   end
--- app/models/wechat/model/menu_organ.rb
+++ app/models/wechat/model/menu_organ.rb
@@ -3,14 +3,13 @@ module Wechat
     extend ActiveSupport::Concern
 
     included do
-      attribute :appid, :string, index: true
       attribute :name, :string
 
+      belongs_to :menu
       belongs_to :organ, class_name: 'Org::Organ', optional: true
 
       belongs_to :scene, optional: true
       belongs_to :tag, optional: true
-      belongs_to :menu
     end
 
   end
