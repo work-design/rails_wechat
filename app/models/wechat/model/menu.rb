34ae68d5f8657d5b7b88b33d47470e5c4f629001
qinmingyuan <mingyuan0715@foxmail.com>
2023-01-15 16:04:48 +0800

    menu root

--- app/models/wechat/model/menu.rb
+++ app/models/wechat/model/menu.rb
@@ -20,7 +20,7 @@ module Wechat
       has_many :apps, through: :menu_apps
       has_many :scenes, through: :menu_apps
 
-      acts_as_list scope: [:menu_root_id, :organ_id]
+      acts_as_list scope: [:root_position, :organ_id]
 
       after_save_commit :sync_to_wechat, if: -> { (saved_changes.keys & ['name', 'value', 'mp_appid', 'mp_pagepath']).present? }
     end
