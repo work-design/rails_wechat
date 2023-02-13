45444139257feb2f9d15973d37f779fd07717701
qinmingyuan <mingyuan0715@foxmail.com>
2023-02-13 11:28:13 +0800

    active

--- app/apis/wechat/api/provider/license.rb
+++ app/apis/wechat/api/provider/license.rb
@@ -3,11 +3,11 @@ module Wechat::Api
     BASE = 'https://qyapi.weixin.qq.com/cgi-bin/license/'
 
     def active_info_by_code(corpid, active_code)
-      provider_get 'get_active_info_by_code', params: { corpid: corpid, active_code: active_code }, origin: BASE
+      provider_post 'get_active_info_by_code', corpid: corpid, active_code: active_code, origin: BASE
     end
 
     def list_actived_account(corpid, **options)
-      provider_get 'list_actived_account', params: { corpid: corpid }, origin: BASE, **options
+      provider_post 'list_actived_account', corpid: corpid, origin: BASE, **options
     end
 
   end
