cbfbf34b9852403d8d7e26dff571c5948fed7601
qinmingyuan <mingyuan0715@foxmail.com>
2023-03-04 20:12:45 +0800

    query

--- app/apis/wx_pay/sign/hmac.rb
+++ app/apis/wx_pay/sign/hmac.rb
@@ -5,7 +5,9 @@ module WxPay::Sign
     extend self
 
     def generate(params, key:)
-      query = params.compact_blank.sort.to_query
+      query = params.compact_blank.sort.map.each do |k, v|
+        "#{k}=#{v}"
+      end.join('&')
       string_sign_temp = "#{query}&key=#{key}"
 
       OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'), key, string_sign_temp).upcase
