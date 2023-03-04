c4780d25f75c42dee9e2b030f3eb6b35f2dc20de
qinmingyuan <mingyuan0715@foxmail.com>
2023-03-04 20:37:55 +0800

    use read key

--- app/apis/wx_pay/api/mch.rb
+++ app/apis/wx_pay/api/mch.rb
@@ -89,7 +89,7 @@ module WxPay::Api
         auth_code: auth_code,
         **v2_common_payee_params
       }
-      opts.merge! sign: WxPay::Sign::Hmac.generate(opts, key: @payee.apiclient_key)
+      opts.merge! sign: WxPay::Sign::Hmac.generate(opts, key: @payee.key)
 
       r = @client.with_options(origin: BASE, debug_level: 2).post('pay/micropay', body: opts.to_xml(root: 'xml', skip_types: true, skip_instruct: true))
       Hash.from_xml r.to_s
