dc4c588352d14ca301fec6e5245304d291c498f4
qinmingyuan <mingyuan0715@foxmail.com>
2023-03-15 16:11:52 +0800

    both open corpid

--- app/models/wechat/model/provider_organ.rb
+++ app/models/wechat/model/provider_organ.rb
@@ -8,7 +8,7 @@ module Wechat
 
       belongs_to :organ, class_name: 'Org::Organ', foreign_key: :corpid, primary_key: :corpid, inverse_of: :provider_organs
       belongs_to :provider
-      has_many :corps, primary_key: :open_corpid, foreign_key: :corp_id, inverse_of: :provider_organs
+      has_many :corps, primary_key: :open_corpid, foreign_key: :open_corpid, inverse_of: :provider_organs
 
       after_save_commit :compute_open_corpid, if: -> { saved_change_to_corpid? }
     end
