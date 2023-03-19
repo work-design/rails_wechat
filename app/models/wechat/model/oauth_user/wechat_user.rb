0aff511b5c6cbc49bca13a111e6d8e0d9ed22433
qinmingyuan <mingyuan0715@foxmail.com>
2023-03-19 12:49:17 +0800

    init member inviter

--- app/models/wechat/model/oauth_user/wechat_user.rb
+++ app/models/wechat/model/oauth_user/wechat_user.rb
@@ -28,6 +28,7 @@ module Wechat
 
       before_save :auto_link, if: -> { unionid.present? && unionid_changed? }
       after_save_commit :sync_remark_later, if: -> { saved_change_to_remark? }
+      after_save_commit :auto_join_organ, if: -> { member_inviter && saved_change_to_member_inviter_id? }
       after_save_commit :prune_user_tags, if: -> { unsubscribe_at.present? && saved_change_to_unsubscribe_at? }
     end
 
@@ -41,6 +42,10 @@ module Wechat
       self.identity ||= same_oauth_user.identity
       self.name ||= same_oauth_user.name
       self.avatar_url ||= same_oauth_user.avatar_url
+    end
+
+    def auto_join_organ
+      members.find_by(organ_id: member_inviter.organ_id) || members.create(organ_id: member_inviter.organ_id, state: 'pending_trial')
     end
 
     def sync_remark_later
