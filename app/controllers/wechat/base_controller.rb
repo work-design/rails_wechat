c54d4d42ba4c0f18b7677df3a904f9d6582517a2
qinmingyuan <mingyuan0715@foxmail.com>
2023-03-26 19:13:53 +0800

    test with state visit

--- app/controllers/wechat/base_controller.rb
+++ app/controllers/wechat/base_controller.rb
@@ -4,14 +4,14 @@ module Wechat
 
     private
     def xxx(oauth_user)
-      state_hash = urlsafe_decode64(params[:state])
+      state = Com::State.find(params[:state])
 
       oauth_user.generate_account! unless oauth_user.user
       login_by_oauth_user(oauth_user)
 
       Com::SessionChannel.broadcast_to(params[:state], auth_token: current_authorized_token.id)
-      url = url_for(auth_token: current_authorized_token.id, **state_hash.except(:auth_token))
-      redirect_to url, allow_other_host: true
+
+      render 'state_visit', layout: 'raw', locals: { state: state, auth_token: current_authorized_token.id }
     end
 
   end
