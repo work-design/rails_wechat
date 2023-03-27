6be3c859483b6a07ef2d3449c76f87df4ad69d5f
qinmingyuan <mingyuan0715@foxmail.com>
2023-03-27 16:06:34 +0800

    direct for access

--- app/controllers/wechat/base_controller.rb
+++ app/controllers/wechat/base_controller.rb
@@ -11,6 +11,11 @@ module Wechat
 
       Com::SessionChannel.broadcast_to(params[:state], auth_token: current_authorized_token.id)
 
+      response.headers['Access-Control-Allow-Origin'] = request.origin
+      response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+      response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-Csrf-Token, X-User-Token, X-User-Email'
+      response.headers['Access-Control-Max-Age'] = '1728000'
+      response.headers['Access-Control-Allow-Credentials'] = true
       render 'state_visit', layout: 'raw', locals: { state: state, auth_token: current_authorized_token.id }
     end
 
