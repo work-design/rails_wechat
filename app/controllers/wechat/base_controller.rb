a952c8b2c84aee7cb8c712dd0e70d48a65573e10
qinmingyuan <mingyuan0715@foxmail.com>
2023-03-29 23:13:33 +0800

    gem

--- app/controllers/wechat/base_controller.rb
+++ app/controllers/wechat/base_controller.rb
@@ -4,10 +4,10 @@ module Wechat
 
     private
     def xxx(oauth_user)
-      state = Com::State.find(params[:state])
+      state = Com::State.find_by(id: params[:state])
 
       oauth_user.generate_account! unless oauth_user.user
-      state.update user_id: oauth_user.user&.id
+      state.update user_id: oauth_user.user&.id if state
       login_by_oauth_user(oauth_user)
 
       Com::SessionChannel.broadcast_to(params[:state], auth_token: current_authorized_token.id)
@@ -17,7 +17,12 @@ module Wechat
       response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-Csrf-Token, X-User-Token, X-User-Email'
       response.headers['Access-Control-Max-Age'] = '1728000'
       response.headers['Access-Control-Allow-Credentials'] = true
-      render 'state_visit', layout: 'raw', locals: { state: state, auth_token: current_authorized_token.id }
+
+      if state
+        render 'state_visit', layout: 'raw', locals: { state: state, auth_token: current_authorized_token.id }
+      else
+        render 'visit', layout: 'raw', locals: { url: url_for(controller: '/home') }
+      end
     end
 
   end
