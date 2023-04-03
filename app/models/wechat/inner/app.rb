a9f782aa5f99409228eda61341a62423df758d66
qinmingyuan <mingyuan0715@foxmail.com>
2023-04-03 14:15:58 +0800

    sync default menu

--- app/models/wechat/inner/app.rb
+++ app/models/wechat/inner/app.rb
@@ -6,6 +6,7 @@ module Wechat
       has_many :menu_apps, -> { where(scene_id: nil) }, primary_key: :appid, foreign_key: :appid
       has_many :menus, primary_key: :appid, foreign_key: :appid
       has_many :all_menu_apps, class_name: 'MenuApp', primary_key: :appid, foreign_key: :appid
+      has_many :pure_menu_roots, class_name: 'MenuRoot', primary_key: :appid, foreign_key: :appid
       has_many :receives, primary_key: :appid, foreign_key: :appid
       has_many :replies, primary_key: :appid, foreign_key: :appid
       has_many :requests, primary_key: :appid, foreign_key: :appid
@@ -22,13 +23,19 @@ module Wechat
     def sync_from_menu
       r = api.menu
       present_menus = r.dig('menu', 'button')
-      present_menus.each do |present_menu|
+      present_menus.each_with_index do |present_menu, index|
         if present_menu['sub_button'].present?
-          parent = self.menus.build(type: 'ParentMenu', name: present_menu['name'])
+          parent = self.pure_menu_roots.find_or_initialize_by(position: index + 1)
+          parent.name = present_menu['name']
           present_menu['sub_button'].each do |sub|
-            parent.children.build(appid: appid, name: sub['name'], menu_type: sub['type'], value: sub['url'] || sub['key'])
+            m = parent.menus.find_or_initialize_by(appid: appid)
+            m.name = sub['name']
+            #m.type = sub['type']
+            m.value = sub['url'] || sub['key']
           end
           parent.save
+        else
+
         end
       end
     end
