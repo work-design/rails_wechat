6371a7c93e727fe85d65078cbbf3afae3a4283b2
qinmingyuan <mingyuan0715@foxmail.com>
2023-04-19 22:28:18 +0800

    default subject type 0

--- app/models/wechat/model/oauth_user/wechat_user.rb
+++ app/models/wechat/model/oauth_user/wechat_user.rb
@@ -130,7 +130,7 @@ module Wechat
       user_tags.update_all(synced: false)
     end
 
-    def get_external_userid!(corp: app.organ.corps[0], subject_type: 1)
+    def get_external_userid!(corp: app.organ.corps[0], subject_type: 0)
       return if unionid.blank?
       r = app.get_external_userid(unionid, uid, corp: corp, subject_type: subject_type)
       logger.debug "\e[35m  External Userid: #{r}  \e[0m"
