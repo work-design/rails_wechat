df0c96f5f6f080d9a43a9827d34a42b95da68f23
qinmingyuan <mingyuan0715@foxmail.com>
2023-04-19 09:50:33 +0800

    improve payee api

--- app/models/wechat/model/payee/mch_payee.rb
+++ app/models/wechat/model/payee/mch_payee.rb
@@ -3,7 +3,17 @@ module Wechat
     extend ActiveSupport::Concern
 
     included do
-      belongs_to :parter_payee, foreign_key: :mch_id, primary_key: :mch_id, optional: true
+      belongs_to :partner_payee, foreign_key: :mch_id, primary_key: :mch_id, optional: true
+    end
+
+    def api
+      return @api if defined? @api
+
+      if partner_payee
+        @api = WxPay::Api::Partner.new(payee: partner_payee, partner: partner_payee.partner)
+      else
+        @api = WxPay::Api::Mch.new(payee: self)
+      end
     end
 
   end
