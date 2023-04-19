c6199e78f8044e16a843d64a484fd9d6ef48fda9
qinmingyuan <mingyuan0715@foxmail.com>
2023-04-19 12:27:24 +0800

    payee

--- app/models/wechat/ext/organ_domain.rb
+++ app/models/wechat/ext/organ_domain.rb
@@ -7,7 +7,7 @@ module Wechat
       has_one :app, class_name: 'Wechat::PublicApp', primary_key: :identifier, foreign_key: :domain
 
       has_many :payee_domains, class_name: 'Wechat::PayeeDomain', primary_key: :identifier, foreign_key: :domain
-      has_many :payees, through: :payee_domains
+      has_many :payees, class_name: 'Wechat::Payee', through: :payee_domains
     end
 
   end
