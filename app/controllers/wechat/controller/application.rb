1250b2f2dbd5afa9bd6f6521af0e668647cf7360
qinmingyuan <mingyuan0715@foxmail.com>
2023-04-19 12:00:52 +0800

    use payee

--- app/controllers/wechat/controller/application.rb
+++ app/controllers/wechat/controller/application.rb
@@ -69,13 +69,7 @@ module Wechat
     def current_payee
       return @current_payee if defined?(@current_payee)
 
-      if params[:appid]
-        @current_payee = current_organ_domain.payee_apps.enabled.find_by(appid: params[:appid])
-      elsif current_wechat_app
-        @current_payee = current_organ_domain.payee_apps.enabled.find_by(appid: current_wechat_app.appid)
-      else
-        @current_payee = current_organ_domain.payee_apps.take
-      end
+      @current_payee = current_organ_domain.payees.take
 
       logger.debug "\e[35m  Current Payee: #{@current_payee&.id}  \e[0m"
       @current_payee
