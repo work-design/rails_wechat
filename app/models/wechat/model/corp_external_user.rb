02a0f41212806d29296237c64db8fe9788fadad1
qinmingyuan <mingyuan0715@foxmail.com>
2023-04-21 23:21:53 +0800

    inti project type

--- app/models/wechat/model/corp_external_user.rb
+++ app/models/wechat/model/corp_external_user.rb
@@ -6,13 +6,18 @@ module Wechat
       attribute :uid, :string, index: true
       attribute :unionid, :string, index: true
       attribute :external_userid, :string, index: true
-      attribute :provider_external_userid, :string
       attribute :pending_id, :string
       attribute :corp_id, :string, index: true
+
+      enum subject_type: {
+        oneself: 0,
+        provider: 1
+      }
 
       has_one :corp, primary_key: :corp_id, foreign_key: :corp_id
       belongs_to :wechat_user, foreign_key: :uid, primary_key: :uid
 
+      before_validation :init_subject_type, if: -> { uid.present? }
       after_create_commit :get_external_userid_later
     end
 
@@ -20,16 +25,19 @@ module Wechat
       CorpExternalUserJob.perform_later(self)
     end
 
+    def init_subject_type
+      if wechat_user.app.global
+        self.subject_type = 1
+      else
+        self.subject_type = 0
+      end
+    end
+
     def get_external_userid!
-      r = corp.get_external_userid(unionid, uid, subject_type: 0)
+      r = corp.get_external_userid(unionid, uid, subject_type: subject_type)
       logger.debug "\e[35m  External Userid: #{r}  \e[0m"
       self.external_userid = r['external_userid']
       self.pending_id = r['pending_id']
-
-      r1 = corp.get_external_userid(unionid, uid, subject_type: 1)
-      logger.debug "\e[35m  Provider External Userid: #{r1}  \e[0m"
-      self.provider_external_userid = r1['external_userid']
-
       self.save
     end
 
