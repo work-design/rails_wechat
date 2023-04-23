a4cd73f6e31e7e4c1c003a4d4dfee7ad6afa03b9
qinmingyuan <mingyuan0715@foxmail.com>
2023-04-23 11:07:37 +0800

    maintain id

--- app/controllers/wechat/admin/organs_controller.rb
+++ app/controllers/wechat/admin/organs_controller.rb
@@ -1,7 +1,7 @@
 module Wechat
   class Admin::OrgansController < Admin::BaseController
     before_action :set_organ
-    before_action :set_corp_users, only: [:edit, :update]
+    before_action :set_apps, only: [:edit, :update]
 
     def show
     end
@@ -11,18 +11,16 @@ module Wechat
       @organ = current_organ
     end
 
-    def set_corp_users
-      if RailsWechat.config.suite_id.present?
-        @corp_users = CorpUser.where(suite_id: RailsWechat.config.suite_id, corp_id: @organ.corpid)
-      else
-        @corp_users = CorpUser.none
-      end
+    def set_apps
+      q_params = {}
+      q_params.merge! organ_id: current_organ.self_and_ancestor_ids
+
+      @apps = PublicApp.default_where(q_params)
     end
 
     def organ_params
       params.fetch(:organ, {}).permit(
-        :corpid,
-        :corp_user_id
+        :appid
       )
     end
 
--- app/models/wechat/ext/organ.rb
+++ app/models/wechat/ext/organ.rb
@@ -5,11 +5,13 @@ module Wechat
     included do
       attribute :limit_wechat, :integer, default: 1
       attribute :limit_wechat_menu, :integer, default: 1
+      attribute :appid, :string
+
+      belongs_to :app, class_name: 'Wechat::App', foreign_key: :appid, primary_key: :appid, optional: true
+      belongs_to :corp_user, class_name: 'Wechat::CorpUser', optional: true
 
       has_many :apps, class_name: 'Wechat::App', dependent: :destroy_async
       has_many :corps, class_name: 'Wechat::Corp', dependent: :nullify
-
-      belongs_to :corp_user, class_name: 'Wechat::CorpUser', optional: true
 
       validates :limit_wechat_menu, inclusion: { in: [0, 1, 2, 3] }
     end
