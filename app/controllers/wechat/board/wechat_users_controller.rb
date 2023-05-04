3799e829ca1346f95bb2716116738b37a68f13c0
qinmingyuan <mingyuan0715@foxmail.com>
2023-05-04 23:59:50 +0800

    wechat users

---
+++ app/controllers/wechat/board/wechat_users_controller.rb
@@ -0,0 +1,46 @@
+module Wechat
+  class Board::WechatUsersController < Board::BaseController
+    before_action :set_user
+    before_action :set_oauth_user, only: [:show, :edit, :update, :destroy, :actions]
+
+    def index
+      @oauth_users = current_user.wechat_users.includes(:app).order(appid: :asc)
+    end
+
+    def create
+      @oauth_user = OauthUser.find_or_initialize_by(type: oauth_user_params[:type], uid: oauth_user_params[:uid])
+      @oauth_user.save_info(oauth_user_params)
+      @oauth_user.init_user
+
+      if @oauth_user.save
+        login_by_account @oauth_user.account
+        render json: { oauth_user: @oauth_user.as_json, user: @oauth_user.user.as_json }
+      end
+    end
+
+    def bind
+      @oauth_user = OauthUser.find_by(uid: params[:uid])
+      @oauth_user.account = current_account
+
+      @oauth_user.save
+
+      redirect_to board_root_url
+    end
+
+    private
+    def set_oauth_user
+      @oauth_user = OauthUser.find(params[:id])
+    end
+
+    def oauth_user_params
+      params.fetch(:oauth_user, {}).permit(
+        :uid,
+        :provider,
+        :type,
+        :name,
+        :access_token
+      )
+    end
+
+  end
+end
