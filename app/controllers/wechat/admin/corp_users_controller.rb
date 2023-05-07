496da1b5159559764958f55a03d6814cd8add294
qinmingyuan <mingyuan0715@foxmail.com>
2023-05-07 22:48:53 +0800

    the logic for agent

--- app/controllers/wechat/admin/corp_users_controller.rb
+++ app/controllers/wechat/admin/corp_users_controller.rb
@@ -1,15 +1,19 @@
 module Wechat
   class Admin::CorpUsersController < Admin::BaseController
-    before_action :set_app
+    before_action :set_agent
     before_action :set_corp_user, only: [:show, :edit, :update, :destroy]
 
     def index
-      @corp_users = @app.corp_users.page(params[:page])
+      @corp_users = @agent.corp_users.page(params[:page])
     end
 
     private
+    def set_agent
+      @agent = Agent.find params[:agent_id]
+    end
+
     def set_corp_user
-      @corp_user = @app.corp_users.find params[:id]
+      @corp_user = @agent.corp_users.find params[:id]
     end
 
   end
