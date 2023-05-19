89f4f210e0f00d68037debcec32383abf461b19c
qinmingyuan <mingyuan0715@foxmail.com>
2023-05-19 19:45:02 +0800

    juse oauth2 url

--- app/controllers/wechat/controller/application.rb
+++ app/controllers/wechat/controller/application.rb
@@ -22,7 +22,7 @@ module Wechat
       elsif request.variant.include?(:wechat)
         return if current_wechat_user && current_user
 
-        if current_oauth_app.oauth_enable && current_oauth_app.respond_to?(:oauth2_url)
+        if current_oauth_app.respond_to?(:oauth2_url)
           url = current_oauth_app.oauth2_url(state: urlsafe_encode64(destroyable: false), port: request.port, protocol: request.protocol)
           logger.debug "\e[35m  Redirect to: #{url}  \e[0m"
           redirect_to url, allow_other_host: true and return
