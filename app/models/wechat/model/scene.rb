901242fd7f675ed4707dc01ede7e70b9bf7ae662
qinmingyuan <mingyuan0715@foxmail.com>
2023-06-27 17:12:53 +0800

    simple the refresh logic

--- app/models/wechat/model/scene.rb
+++ app/models/wechat/model/scene.rb
@@ -33,7 +33,6 @@ module Wechat
       has_one_attached :qrcode
 
       after_initialize :init_match_value, if: -> { new_record? && handle }
-      before_validation :init_expire_at, if: -> { expire_seconds.present? && expire_seconds_changed? }
       before_validation :sync_from_app, if: -> { organ_id.blank? && appid.present? && appid_changed? }
       after_save_commit :to_qrcode!, if: -> { saved_change_to_match_value? }
       after_save_commit :refresh_when_expired, if: -> { saved_change_to_expire_at? }
@@ -41,10 +40,6 @@ module Wechat
 
     def init_match_value
       self.match_value = "#{handle_type.downcase.gsub('::', '_')}_#{handle_id}_#{organ_id}"
-    end
-
-    def init_expire_at
-      self.expire_at ||= Time.current + expire_seconds
     end
 
     def sync_from_app
@@ -96,6 +91,7 @@ module Wechat
       end
       self.qrcode_ticket = r['ticket']
       self.qrcode_url = r['url']
+      self.expire_at = Time.current + r['expire_seconds']
       r
     end
 
@@ -139,14 +135,9 @@ module Wechat
 
     def refresh!(now = false)
       if expired? || now
-        self.expire_at = Time.current + expire_seconds
         to_qrcode!
       end
       self
-    end
-
-    def refresh
-      self.expire_at = Time.current + expire_seconds
     end
 
     def sync_menu
