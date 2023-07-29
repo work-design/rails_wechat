ae1ef0e3280e78f40f94ed936dc6f8c7e5aa402a
qinmingyuan <mingyuan0715@foxmail.com>
2023-07-29 23:53:11 +0800

    organ host

--- app/models/wechat/model/menu/view_menu.rb
+++ app/models/wechat/model/menu/view_menu.rb
@@ -4,9 +4,9 @@ module Wechat
 
     def as_json(options = {})
       if options[:app]
-        host = options[:app].domain.presence || options[:app].organ.domain.presence || Rails.application.routes.default_url_options[:host]
+        host = options[:app].domain.presence || options[:app].organ.host.presence || Rails.application.routes.default_url_options[:host]
       else
-        host = app&.domain.presence || organ&.domain.prescen || Rails.application.routes.default_url_options[:host]
+        host = app&.domain.presence || organ&.host.prescen || Rails.application.routes.default_url_options[:host]
       end
       r = URI(value)
       r.host = host
