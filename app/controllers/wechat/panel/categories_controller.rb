176b3e09e7582c83a9ba511611b8ab147653942f
qinmingyuan <mingyuan0715@foxmail.com>
2023-08-04 17:19:13 +0800

    parent id

--- app/controllers/wechat/panel/categories_controller.rb
+++ app/controllers/wechat/panel/categories_controller.rb
@@ -1,5 +1,12 @@
 module Wechat
   class Panel::CategoriesController < Panel::BaseController
 
+    def index
+      q_params = {}
+      q_params.merge! params.permit('name-like')
+
+      @categories = Category.default_where(q_params).page(params[:page])
+    end
+
   end
 end
