979b4edd58bb96274eba92d200e7b6dbdb3a1283
qinmingyuan <mingyuan0715@foxmail.com>
2023-08-06 15:05:07 +0800

    shared apps

--- app/controllers/wechat/in/apps_controller.rb
+++ app/controllers/wechat/in/apps_controller.rb
@@ -3,7 +3,9 @@ module Wechat
     before_action :set_app, only: [:show]
 
     def index
-      q_params = {}
+      q_params = {
+        organ_id: current_organ.provider_id + current_organ.self_and_ancestor_ids
+      }
       q_params.merge! params.permit(:id)
 
       @apps = App.default_where(q_params).order(id: :asc).page(params[:page])
