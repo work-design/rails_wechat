dcfc0b5ca8d5393b6513614e9f89c03ed4c20813
qinmingyuan <mingyuan0715@foxmail.com>
2023-08-07 00:29:24 +0800

    simple agency logic

--- app/models/wechat/model/service.rb
+++ app/models/wechat/model/service.rb
@@ -13,18 +13,17 @@ module Wechat
       belongs_to :request, optional: true
       belongs_to :app, foreign_key: :appid, primary_key: :appid, optional: true
       belongs_to :wechat_user, foreign_key: :open_id, primary_key: :uid, optional: true
-      belongs_to :agency, foreign_key: :appid, primary_key: :appid, optional: true
 
-      after_initialize if: :new_record? do
-        if request
-          self.appid = request.appid
-          self.open_id = request.open_id
-        end
-      end
+      after_initialize :init_open, if: -> { new_record? && request }
+    end
+
+    def init_open
+      self.appid = request.appid
+      self.open_id = request.open_id
     end
 
     def do_send
-      agency.api.message_custom_send to_wechat
+      app.api.message_custom_send to_wechat
     end
 
     def content
