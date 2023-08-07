58ff2fd3c0f70f847fca0e1c2ad4d9d2707140db
qinmingyuan <mingyuan0715@foxmail.com>
2023-08-07 16:34:28 +0800

    menu roots

--- app/controllers/wechat/admin/menu_roots_controller.rb
+++ app/controllers/wechat/admin/menu_roots_controller.rb
@@ -2,6 +2,10 @@ module Wechat
   class Admin::MenuRootsController < Admin::BaseController
     before_action :set_menu_root, only: [:new, :create]
 
+    def index
+      @menu_roots = MenuRoot.where(organ_id: nil)
+    end
+
     private
     def set_menu_root
       @menu_root = MenuRoot.where(position: params[:position]).find_or_initialize_by(organ_id: current_organ.id)
