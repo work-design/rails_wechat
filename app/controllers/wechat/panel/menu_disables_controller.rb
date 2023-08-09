004e052fa99b8d7e0e6fe6749d05f65a82d5684e
qinmingyuan <mingyuan0715@foxmail.com>
2023-08-09 16:20:25 +0800

    make menu disable works

--- app/controllers/wechat/panel/menu_disables_controller.rb
+++ app/controllers/wechat/panel/menu_disables_controller.rb
@@ -1,15 +1,21 @@
 module Wechat
   class Panel::MenuDisablesController < Panel::BaseController
     before_action :set_app
-    before_action :set_menu_disable, only: [:show, :edit, :update, :destroy]
+    before_action :set_menu
+    before_action :set_menu_disable, only: [:destroy]
+    before_action :set_new_menu_disable, only: [:create]
 
     private
+    def set_new_menu_disable
+      @menu_disable = @app.menu_disables.create(menu_id: @menu.id)
+    end
+
     def set_menu_disable
-      @menu_disable = MenuDisable.find(params[:id])
+      @menu_disable = @app.menu_disables.find(params[:id])
     end
 
     def set_menu
-      @menu = @menu_root.menus.build(appid: params[:appid])
+      @menu = Menu.find params[:menu_id]
     end
 
   end
