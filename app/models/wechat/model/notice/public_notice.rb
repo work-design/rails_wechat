694d13fc06fa51336ff88103bc78385798256e94
qinmingyuan <mingyuan0715@foxmail.com>
2023-08-11 17:26:08 +0800

    send method

--- app/models/wechat/model/notice/public_notice.rb
+++ app/models/wechat/model/notice/public_notice.rb
@@ -3,7 +3,7 @@ module Wechat
     BASE = 'https://api.weixin.qq.com/cgi-bin/'
 
     def do_send
-      r = app.api.post 'message/template/send', **message_hash, base: BASE
+      r = app.api.post 'message/template/send', **message_hash, origin: BASE
       if r['errcode'] == 0
         self.update msg_id: r['msgid']
         subscribe.update sending_at: Time.now if subscribe
