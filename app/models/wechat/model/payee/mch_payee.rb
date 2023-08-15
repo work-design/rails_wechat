d3d07dad9ab8611dfec592d5f51367f4837f85eb
qinmingyuan <mingyuan0715@foxmail.com>
2023-08-16 00:10:42 +0800

    belongs to wechat app

--- app/models/wechat/model/payee/mch_payee.rb
+++ app/models/wechat/model/payee/mch_payee.rb
@@ -2,18 +2,9 @@ module Wechat
   module Model::Payee::MchPayee
     extend ActiveSupport::Concern
 
-    included do
-      belongs_to :partner_payee, foreign_key: :mch_id, primary_key: :mch_id, optional: true
-    end
-
     def api
       return @api if defined? @api
-
-      if partner_payee
-        @api = WxPay::Api::Partner.new(payee: partner_payee, partner: partner_payee.partner)
-      else
-        @api = WxPay::Api::Mch.new(payee: self)
-      end
+      @api = WxPay::Api::Mch.new(payee: self)
     end
 
   end
