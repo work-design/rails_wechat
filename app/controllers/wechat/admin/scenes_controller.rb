591c97b42e6ef44f068d84e31ad04596df011530
qinmingyuan <mingyuan0715@foxmail.com>
2023-08-16 18:18:21 +0800

    share scenes in panel

--- app/controllers/wechat/admin/scenes_controller.rb
+++ app/controllers/wechat/admin/scenes_controller.rb
@@ -1,38 +1,6 @@
 module Wechat
-  class Admin::ScenesController < Admin::BaseController
-    before_action :set_app
-    before_action :set_scene, only: [:show, :edit, :update, :destroy, :actions]
-    before_action :set_new_scene, only: [:new, :create]
-
-    def index
-      q_params = {}
-      q_params.merge! default_params
-      q_params.merge! params.permit(:handle_type, :handle_id)
-
-      @scenes = @app.scenes.default_where(q_params).order(id: :desc).page(params[:page])
-    end
-
-    def sync
-      r= @app.api.menu_create @app.menu
-      render 'sync', locals: { notice: r.to_s }
-    end
-
-    private
-    def set_scene
-      @scene = @app.scenes.find(params[:id])
-    end
-
-    def set_new_scene
-      @scene = @app.scenes.build(scene_params)
-    end
-
-    def scene_params
-      p = params.fetch(:scene, {}).permit(
-        :match_value,
-        :expire_seconds
-      )
-      p.merge! default_form_params
-    end
+  class Admin::ScenesController < Panel::ScenesController
+    include Controller::Admin
 
   end
 end
---
+++ app/controllers/wechat/panel/scenes_controller.rb
@@ -0,0 +1,38 @@
+module Wechat
+  class Panel::ScenesController < Panel::BaseController
+    before_action :set_app
+    before_action :set_scene, only: [:show, :edit, :update, :destroy, :actions]
+    before_action :set_new_scene, only: [:new, :create]
+
+    def index
+      q_params = {}
+      q_params.merge! default_params
+      q_params.merge! params.permit(:handle_type, :handle_id)
+
+      @scenes = @app.scenes.default_where(q_params).order(id: :desc).page(params[:page])
+    end
+
+    def sync
+      r= @app.api.menu_create @app.menu
+      render 'sync', locals: { notice: r.to_s }
+    end
+
+    private
+    def set_scene
+      @scene = @app.scenes.find(params[:id])
+    end
+
+    def set_new_scene
+      @scene = @app.scenes.build(scene_params)
+    end
+
+    def scene_params
+      p = params.fetch(:scene, {}).permit(
+        :match_value,
+        :expire_seconds
+      )
+      p.merge! default_form_params
+    end
+
+  end
+end
