e000d269d810d3383871c52137f960df0d469202
qinmingyuan <mingyuan0715@foxmail.com>
2023-08-18 15:24:49 +0800

    support fitler with appid

--- app/controllers/wechat/panel/platform_receives_controller.rb
+++ app/controllers/wechat/panel/platform_receives_controller.rb
@@ -3,7 +3,10 @@ module Wechat
     before_action :set_platform
 
     def index
-      @platform_receives = @platform.receives.order(id: :desc).page(params[:page])
+      q_params = {}
+      q_params.merge! params.permit(:appid, :open_id)
+
+      @platform_receives = @platform.receives.default_where(q_params).order(id: :desc).page(params[:page])
     end
 
     private
