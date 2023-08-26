8e354537f07e64642666f5ce9fb709830b59e902
qinmingyuan <mingyuan0715@foxmail.com>
2023-08-26 18:40:37 +0800

    resource organs

---
+++ app/controllers/wechat/panel/organs_controller.rb
@@ -0,0 +1,40 @@
+module Wechat
+  class Panel::OrgansController < Panel::BaseController
+    before_action :set_organ
+    before_action :set_apps, only: [:edit, :update]
+    before_action :set_agencies, only: [:edit, :update]
+
+    def index
+      @organs = Org::Organ.where.associated(:apps).page(params[:page])
+    end
+
+    def show
+    end
+
+    private
+    def set_organ
+      @organ = current_organ
+    end
+
+    def set_apps
+      q_params = {}
+      q_params.merge! organ_id: current_organ.self_and_ancestor_ids
+
+      @apps = PublicApp.default_where(q_params)
+    end
+
+    def set_agencies
+      q_params = {}
+      q_params.merge! organ_id: current_organ.self_and_ancestor_ids
+
+      @agencies = PublicAgency.default_where(q_params)
+    end
+
+    def organ_params
+      params.fetch(:organ, {}).permit(
+        :appid
+      )
+    end
+
+  end
+end
