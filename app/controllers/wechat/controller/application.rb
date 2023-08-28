0d7ef9053ef44b4609fd074476770575f947a12a
qinmingyuan <mingyuan0715@foxmail.com>
2023-08-28 22:16:40 +0800

    corps

--- app/controllers/wechat/controller/application.rb
+++ app/controllers/wechat/controller/application.rb
@@ -46,7 +46,7 @@ module Wechat
     def current_oauth_app
       return @current_oauth_app if defined? @current_oauth_app
       if request.variant.include?(:work_wechat)
-        @current_oauth_app = Agent.default_where(default_ancestors_params).take || current_organ.corps.enabled.take
+        @current_oauth_app = current_organ.corps.enabled.take
       else
         @current_oauth_app = current_organ.app || current_organ.provider.app
       end
