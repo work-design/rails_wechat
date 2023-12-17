f3aad298c07489861a19c3418b79861bebbf7673
qinmingyuan <mingyuan0715@foxmail.com>
2023-12-17 20:26:06 +0800

    support skip user

--- app/controllers/wechat/controller/admin.rb
+++ app/controllers/wechat/controller/admin.rb
@@ -1,5 +1,10 @@
 module Wechat
   module Controller::Admin
+    extend ActiveSupport::Concern
+
+    included do
+      skip_before_action :require_user if whether_filter(:require_user)
+    end
 
     def set_app
       @app = current_organ.apps.find_by(appid: params[:app_appid])
