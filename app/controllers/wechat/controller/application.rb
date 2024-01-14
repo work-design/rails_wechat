a7040c8ff1d9c774f713b93ea26815b2e80063f5
qinmingyuan <mingyuan0715@foxmail.com>
2024-01-14 20:45:25 +0800

    check oauth user

--- app/controllers/wechat/controller/application.rb
+++ app/controllers/wechat/controller/application.rb
@@ -18,8 +18,8 @@ module Wechat
           url = app.oauth2_url(**wechat_oauth_options)
         end
       elsif request.variant.include?(:mini_program)
-        return if current_wechat_user && current_user
         check_jwt_token if params[:auth_jwt_token]
+        return if current_wechat_user && current_user
         render 'require_program_login', layout: 'raw', locals: { path: 'state_return', state: urlsafe_encode64(destroyable: false) } and return
       elsif request.variant.include?(:wechat) && app
         return if current_wechat_user && current_user
