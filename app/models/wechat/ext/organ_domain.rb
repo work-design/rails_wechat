3b446961feb1f0894218aeef35d0d4b0d1925877
qinmingyuan <mingyuan0715@foxmail.com>
2024-01-20 14:42:51 +0800

    fix organ

--- app/models/wechat/ext/organ_domain.rb
+++ app/models/wechat/ext/organ_domain.rb
@@ -3,10 +3,10 @@ module Wechat
     extend ActiveSupport::Concern
 
     included do
-      has_one :corp, class_name: 'Wechat::Corp', primary_key: :identifier, foreign_key: :host
-      has_one :app, class_name: 'Wechat::PublicApp', primary_key: :identifier, foreign_key: :domain
+      has_one :corp, class_name: 'Wechat::Corp', primary_key: :host, foreign_key: :host
+      has_one :app, class_name: 'Wechat::PublicApp', primary_key: :host, foreign_key: :domain
 
-      has_many :payee_domains, class_name: 'Wechat::PayeeDomain', primary_key: :identifier, foreign_key: :domain
+      has_many :payee_domains, class_name: 'Wechat::PayeeDomain', primary_key: :host, foreign_key: :domain
       has_many :payees, class_name: 'Wechat::Payee', through: :payee_domains
     end
 
