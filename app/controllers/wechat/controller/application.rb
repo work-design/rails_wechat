0f02ed71fb1fbbe4fe12d24ee012f863ff137c11
qinmingyuan <279378275@qq.com>
2024-11-08 20:19:06 +0800

    wechat suer

--- app/controllers/wechat/controller/application.rb
+++ app/controllers/wechat/controller/application.rb
@@ -4,7 +4,7 @@ module Wechat
     extend ActiveSupport::Concern
 
     included do
-      helper_method :current_wechat_app, :current_js_app, :current_corp_user
+      helper_method :current_wechat_app, :current_js_app, :current_corp_user, :current_wechat_user
     end
 
     def require_user(app = current_oauth_app)
