5acd5c6de4c62172f6517314caadae494aa31c80
qinmingyuan <279378275@qq.com>
2024-11-10 20:04:03 +0800

    d

--- app/apis/wx_pay/api/shared.rb
+++ app/apis/wx_pay/api/shared.rb
@@ -17,23 +17,5 @@ module WxPay::Api
       get "/v3/profitsharing/transactions/#{transaction_id}/amounts", origin: BASE
     end
 
-    def pay_micropay(out_trade_no:, auth_code:, body:, total_fee:, spbill_create_ip:)
-      opts = {
-        nonce_str: SecureRandom.hex,
-        sign_type: 'HMAC-SHA256',
-        body: body,
-        out_trade_no: out_trade_no,
-        total_fee: total_fee,
-        spbill_create_ip: spbill_create_ip,
-        auth_code: auth_code,
-        **v2_common_payee_params
-      }
-      opts.merge! sign: WxPay::Sign::Hmac.generate(opts, key: @mch.key)
-
-      r = @client.with_options(origin: BASE, debug: Rails.logger.broadcasts[0].instance_values['logdev'].dev, debug_level: 2)
-          .post('pay/micropay', body: opts.to_xml(root: 'xml', skip_types: true, skip_instruct: true, dasherize: false))
-      Hash.from_xml(r.to_s)['xml']
-    end
-
   end
 end
