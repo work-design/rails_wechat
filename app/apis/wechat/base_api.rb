2df99333f51943bb1f35079d51d7f9883af3ee51
qinmingyuan <mingyuan0715@foxmail.com>
2025-01-11 19:33:34 +0800

    api

--- app/apis/wechat/base_api.rb
+++ app/apis/wechat/base_api.rb
@@ -21,9 +21,9 @@ module Wechat
 
     def get(path, params: {}, headers: {}, origin: nil, debug: nil)
       with_options = { origin: origin }
-      with_options.merge! debug: STDERR, debug_level: 2 if debug
+      with_options.merge! debug: STDOUT, debug_level: 2 if debug
 
-      with_access_token(params: params) do
+      with_access_token(params: params, headers: headers) do
         response = @client.with_headers(headers).with(with_options).get(path, params: params)
         debug ? response : parse_response(response)
       end
@@ -57,7 +57,7 @@ module Wechat
     end
 
     protected
-    def with_access_token(params: {}, tries: 2)
+    def with_access_token(params: {}, headers: {}, tries: 2)
       app.refresh_access_token unless app.access_token_valid?
       yield params.merge!(access_token: app.access_token)
     rescue Wechat::AccessTokenExpiredError
