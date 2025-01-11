e8698fd7e52fdc385344b0b5484a493acadc5370
qinmingyuan <mingyuan0715@foxmail.com>
2025-01-11 19:34:10 +0800

    share method

--- app/apis/wechat/base_api.rb
+++ app/apis/wechat/base_api.rb
@@ -19,16 +19,6 @@ module Wechat
       )
     end
 
-    def get(path, params: {}, headers: {}, origin: nil, debug: nil)
-      with_options = { origin: origin }
-      with_options.merge! debug: STDOUT, debug_level: 2 if debug
-
-      with_access_token(params: params, headers: headers) do
-        response = @client.with_headers(headers).with(with_options).get(path, params: params)
-        debug ? response : parse_response(response)
-      end
-    end
-
     def post(path, params: {}, headers: {}, origin: nil, debug: nil, **payload)
       with_options = { origin: origin }
       with_options.merge! debug: STDERR, debug_level: 2 if debug
