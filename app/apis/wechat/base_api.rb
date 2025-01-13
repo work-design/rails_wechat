54d603adf614f6a29bd777e998fa782336bb48f7
qinmingyuan <mingyuan0715@foxmail.com>
2025-01-13 22:09:05 +0800

    spider

--- app/apis/wechat/base_api.rb
+++ app/apis/wechat/base_api.rb
@@ -7,7 +7,7 @@ module Wechat
     include CommonApi
 
     protected
-    def with_access_token(params: {}, headers: {}, tries: 2)
+    def with_access_token(params: {}, headers: {}, payload: {}, tries: 2)
       app.refresh_access_token unless app.access_token_valid?
       params.merge!(access_token: app.access_token)
       yield
