00c1dc27b55ade92d5285ca09c6057e9fee14166
qinmingyuan <mingyuan0715@foxmail.com>
2025-04-16 21:57:45 +0800

    ap

--- app/controllers/wechat/controller/admin.rb
+++ app/controllers/wechat/controller/admin.rb
@@ -4,20 +4,13 @@ module Wechat
     include Roled::Controller::Admin
 
     included do
+      layout -> { 'admin' unless turbo_frame_request? }
       skip_before_action :require_user if whether_filter(:require_user)
     end
 
     private
     def set_app
       @app = current_organ.apps.find_by(appid: params[:app_appid])
-    end
-
-    class_methods do
-
-      private
-      def local_prefixes
-        [controller_path, 'wechat/admin/base']
-      end
     end
 
   end
