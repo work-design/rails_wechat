d515a6ff780277c285ce583956dfd94bb02e19ef
qinmingyuan <mingyuan0715@foxmail.com>
2025-04-28 22:11:05 +0800

    fixlayout frame

--- app/controllers/wechat/controller/admin.rb
+++ app/controllers/wechat/controller/admin.rb
@@ -4,7 +4,7 @@ module Wechat
     include Roled::Controller::Admin
 
     included do
-      layout -> { turbo_frame_body? ? 'frame' : 'admin' }
+      layout -> { turbo_frame_body? ? 'frame_body' : 'admin' }
       skip_before_action :require_user if whether_filter(:require_user)
     end
 
