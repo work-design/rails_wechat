924bcd893b903791505c5a27eb39f7f954d10337
qinmingyuan <mingyuan0715@foxmail.com>
2025-05-01 15:15:44 +0800

    use string

--- app/channels/wechat/session_channel.rb
+++ app/channels/wechat/session_channel.rb
@@ -8,14 +8,13 @@ module Wechat
 
     private
     def refresh_qrcode
-      logger.debug "----------#{params}, #{instance_values}"
       provider_app = organ.provider&.app || App.global.take
       if provider_app
         scene = provider_app.scenes.find_or_initialize_by(match_value: "session_#{session_id}")
         scene.expire_seconds = 600 # 默认 600 秒有效
         scene.check_refresh(true)
         scene.aim = 'login'
-        scene.state_uuid = params[:state]
+        scene.state_uuid = params['state']
         scene.save
 
         broadcast_to(session_id, data_url: scene.qrcode_data_url)
