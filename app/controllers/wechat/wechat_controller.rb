27550e5ec2812cf0bb8f2ed69842f1c1cd0c7630
qinmingyuan <mingyuan0715@foxmail.com>
2025-05-01 12:18:14 +0800

    revert logic

--- app/controllers/wechat/wechat_controller.rb
+++ app/controllers/wechat/wechat_controller.rb
@@ -17,7 +17,11 @@ module Wechat
       end
     end
 
-
+    def user_login
+      @scene = current_oauth_app.scenes.find_by(match_value: "session_#{session.id}")
+      @scene.state_uuid = params[:state].presence || state_enter(destroyable: false, state_controller: 'home', state_action: 'index').id
+      @scene.save
+    end
 
     def admin_login
       if request.subdomain == 'admin'
