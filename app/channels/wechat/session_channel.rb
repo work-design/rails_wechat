987ce7c9ea92ac0510cccfa2f3d60842c108e10b
qinmingyuan <mingyuan0715@foxmail.com>
2025-05-03 19:22:38 +0800

    fix error

--- app/channels/wechat/session_channel.rb
+++ app/channels/wechat/session_channel.rb
@@ -8,7 +8,7 @@ module Wechat
 
     private
     def refresh_qrcode
-      provider_app = organ.provider&.app || App.global.take
+      provider_app = organ&.provider&.app || App.global.take
       if provider_app
         scene = provider_app.scenes.find_or_initialize_by(match_value: "session_#{session_id}")
         scene.expire_seconds = 600 # 默认 600 秒有效
