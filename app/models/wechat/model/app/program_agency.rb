eae61ba853c1c1814dbf932a15b60929042746bf
qinmingyuan <mingyuan0715@foxmail.com>
2025-09-22 14:45:19 +0800

    movedkfulr

--- app/models/wechat/model/app/program_agency.rb
+++ app/models/wechat/model/app/program_agency.rb
@@ -57,8 +57,7 @@ module Wechat
           URI::HTTPS.build(host: computed_webview_domain).to_s,
           URI::HTTPS.build(host: domain).to_s,
           URI::HTTPS.build(host: Rails.application.routes.default_url_options[:host]).to_s,
-          URI::HTTPS.build(host: "org.#{Rails.application.routes.default_url_options[:host]}").to_s,
-          Rails.application.credentials.kf
+          URI::HTTPS.build(host: "org.#{Rails.application.routes.default_url_options[:host]}").to_s
         ].uniq.compact
       }
       api.webview_domain_directly(**h)
